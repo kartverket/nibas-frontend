cff761ade2b211c2bc6c9fa9a35000a091a505f9
Daniel B. Stoa <daniel.stoa@kartverket.no>
2021-11-01 16:24:32 +0100

    Fjerne gammel rest av map constant refaktor
    Former-commit-id: 54dc3f7c3551fa6e5ec654ec767409390184b9bc

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -38,7 +38,7 @@ const Map = ({ backgroundLayersOpen }: Props) => {
     };
   }, []);
 
-  const canEditKommuner = !!map && isLayerVisible(map, "kommuner") && editing;
+  const canEditKommuner = isLayerVisible(map, "kommuner") && editing;
 
   const { visibleLayers, dispatch } = useVisibleLayers();
   const asyncSources = useAsyncSources();
