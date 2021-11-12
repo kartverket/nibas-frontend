7851d87816f3ced8a89e97b18f1a0397367385e4
Daniel B. Stoa <daniel.stoa@kartverket.no>
2021-11-12 08:40:39 +0100

    Gi oss error om ny id legges til som ikke finnes i createLayers
    Former-commit-id: 00cdfd8eca13a17190484be60fc01bd83f46cb4d

--- src/hooks/layers/constants.ts
+++ src/hooks/layers/constants.ts
@@ -1,23 +1,32 @@
 import TileLayer from "ol/layer/Tile";
 import VectorLayer from "ol/layer/Vector";
 import { syncSources } from "hooks/sources/syncSources";
-import { ByLayerId } from "./types";
+import { ByLayerId, LayerId } from "./types";
 
-export const createLayers = () => ({
-  topografiskNorgeskart: new TileLayer({
-    source: syncSources.topografiskNorgeskart,
-  }),
-  administrativeGrenser: new TileLayer({
-    source: syncSources.administrativeGrenser,
-  }),
-  background: new TileLayer({ source: syncSources.background }),
-  vector: new VectorLayer({ source: syncSources.vector }),
-  matrikkelen: new TileLayer({ source: syncSources.matrikkelen }),
-  stedsnavn: new TileLayer({ source: syncSources.stedsnavn }),
-  // ingen source betyr at source settes async
-  fylker: new VectorLayer(),
-  kommuner: new VectorLayer({ minZoom: 11 }),
-});
+// gi oss en error hvis layers ikke inneholder Layer definisjon for alle LayerIds
+type LayerIdGuard<T extends ByLayerId<unknown>> = T[LayerId] extends unknown
+  ? T
+  : never;
+
+export const createLayers = () => {
+  const layers = {
+    topografiskNorgeskart: new TileLayer({
+      source: syncSources.topografiskNorgeskart,
+    }),
+    administrativeGrenser: new TileLayer({
+      source: syncSources.administrativeGrenser,
+    }),
+    background: new TileLayer({ source: syncSources.background }),
+    vector: new VectorLayer({ source: syncSources.vector }),
+    matrikkelen: new TileLayer({ source: syncSources.matrikkelen }),
+    stedsnavn: new TileLayer({ source: syncSources.stedsnavn }),
+    // ingen source betyr at source settes async
+    fylker: new VectorLayer(),
+    kommuner: new VectorLayer({ minZoom: 11 }),
+  };
+
+  return layers as LayerIdGuard<typeof layers>;
+};
 
 export const INITIAL_ZINDEXES: ByLayerId<number> = {
   topografiskNorgeskart: 0,
