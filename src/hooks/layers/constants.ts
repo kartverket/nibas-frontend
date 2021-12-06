fd33d6c96fb0d3f76c27a986cc7d50a49e66aa54
Daniel Stoa <daniel.stoa@kartverket.no>
2021-12-06 12:06:25 +0100

    NIBAS-216: Bestemme z index ut ifra rekkefølge fra createLayers
    Former-commit-id: 02143c893d15156a988394e28550a707e5f3ec39

--- src/hooks/layers/constants.ts
+++ src/hooks/layers/constants.ts
@@ -1,10 +1,10 @@
 import TileLayer from "ol/layer/Tile";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
+import Stroke from "ol/style/Stroke";
+import Style from "ol/style/Style";
 import { ByLayerId, LayerId } from "./types";
 import { syncSources } from "hooks/sources/syncSources";
-import Style from "ol/style/Style";
-import Stroke from "ol/style/Stroke";
 
 // gi oss en error hvis layers ikke inneholder Layer definisjon for alle LayerIds
 type LayerIdGuard<T extends ByLayerId<unknown>> = T[LayerId] extends unknown
@@ -38,16 +38,16 @@ export const createLayers = () => {
   return layers as LayerIdGuard<typeof layers>;
 };
 
-export const INITIAL_ZINDEXES: ByLayerId<number> = {
-  topografiskNorgeskart: 0,
-  administrativeGrenser: 1,
-  fylker: 2,
-  kommuner: 3,
-  stedsnavn: 4,
-  background: -1,
-  matrikkelen: -2,
-  edit: 5,
-};
+// jo senere definert lag, jo høyere z index
+export const INITIAL_ZINDEXES: ByLayerId<number> = Object.keys(
+  createLayers()
+).reduce(
+  (acc, layerId, i) => ({
+    ...acc,
+    [layerId as LayerId]: i,
+  }),
+  {} as ByLayerId<number>
+);
 
 export const INITIAL_VISIBILITY: ByLayerId<boolean> = {
   administrativeGrenser: false,
