3a9849231fe77fe433147c35d9f6691302a90cc3
Daniel Stoa <daniel.stoa@kartverket.no>
2021-12-09 15:38:43 +0100

    NIBAS-127: Bedre skjul/vis funksjonalitet for metadata panel
    Former-commit-id: 08d65da32dd3d2a0add4e2073fba698573a1adaf

--- src/components/Map/MetadataPanel/MetadataPanel.tsx
+++ src/components/Map/MetadataPanel/MetadataPanel.tsx
@@ -1,24 +1,54 @@
+import { useEffect, useState } from "react";
 import { Feature } from "ol";
 import Geometry from "ol/geom/Geometry";
 import styled from "styled-components";
 import { MapInteractable } from "../MapInteractable";
+import Button from "components/Button";
 
 type Props = {
   selectedFeatures: Feature<Geometry>[];
 };
 
 const MetadataPanel = ({ selectedFeatures }: Props) => {
+  const [visible, setVisible] = useState(false);
+  const [singleFeature, setSingleFeature] = useState<Feature<Geometry> | null>(
+    null
+  );
+
+  useEffect(() => {
+    if (selectedFeatures.length === 1) {
+      setSingleFeature(selectedFeatures[0]);
+    } else {
+      setSingleFeature(null);
+    }
+  }, [selectedFeatures]);
+
+  useEffect(() => {
+    setVisible(!!singleFeature);
+  }, [singleFeature]);
+
   // kun vis metadata hvis én feature er selected
   // det gir ikke mening å vise metadata for flere på en gang
-  if (selectedFeatures.length !== 1) return null;
+  if (!visible || !singleFeature) return null;
 
-  const singleFeature = selectedFeatures[0];
-
-  return <Panel>{singleFeature.getProperties().administrativEnhet.navn}</Panel>;
+  return (
+    <Panel>
+      <h3>Linje metadata</h3>
+      {singleFeature.getProperties().administrativEnhet.navn}
+      <Button onClick={() => setVisible(false)}>Close</Button>
+    </Panel>
+  );
 };
 
 const Panel = styled(MapInteractable)`
   grid-area: metadata;
+  border: 1px solid ${({ theme }) => theme.colors.grayLight};
+  padding: 16px;
+
+  > h3 {
+    margin-top: 0;
+    margin-bottom: 16px;
+  }
 `;
 
 export default MetadataPanel;
