9c786c7a59ea449a41497151ce31ed86ae27783b
Daniel Stoa <daniel.stoa@kartverket.no>
2021-12-09 15:36:34 +0100

    NIBAS-225: Fikse App tests så de faktisk passerer
    Former-commit-id: b14ee7eb858c74c6d04d59c7b9d27df44b5ded41

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,8 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("should render without exploding", () => {
+    render(<App />);
+
+    const nibasTitle = screen.getByText("Nasjonal inndelingsbase");
+
+    expect(nibasTitle).toBeInTheDocument();
+  });
 });
