b42bf6b64c54c530fc1caed0af5e8018767f7df7
Daniel Stoa <daniel.stoa@kartverket.no>
2021-12-10 15:56:23 +0100

    NIBAS-224: Flytte providers til egen komponent
    Former-commit-id: ad3fb3d038c5c984aa7772d08079ba3f938c1559

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,11 @@
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { ThemeProvider } from "styled-components";
+import Providers from "./Providers";
 import PageLayout from "components/PageLayout";
-import { defaultTheme } from "style/theme";
 
 const App = () => {
   return (
-    <DndProvider backend={HTML5Backend}>
-      <ThemeProvider theme={defaultTheme}>
-        <PageLayout />
-      </ThemeProvider>
-    </DndProvider>
+    <Providers>
+      <PageLayout />
+    </Providers>
   );
 };
 
