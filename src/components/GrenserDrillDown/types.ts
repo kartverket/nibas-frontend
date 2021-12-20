258b1d68e44453e441bfad368c4862062195cb53
Daniel Stoa <daniel.stoa@kartverket.no>
2021-12-20 13:23:02 +0100

    Fjerne kommentert kode
    Former-commit-id: 86dc17a7a188a7adea959d9d8f5c1c7fedf6e231

--- src/components/GrenserDrillDown/types.ts
+++ src/components/GrenserDrillDown/types.ts
@@ -2,18 +2,6 @@ export type Grense = {
   id: number;
 };
 
-// export type SimpleFylke = Grense & {
-//   type: "FYLKE";
-//   nummer: string;
-//   navn: string;
-// };
-
-// export type SimpleKommune = Grense & {
-//   type: "KOMMUNE";
-//   nummer: string;
-//   navn: string;
-// };
-
 type Spraak = {
   navn: string;
   spraak: string;
