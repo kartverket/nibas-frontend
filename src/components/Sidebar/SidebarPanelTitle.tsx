58a077c7b48b56b0319d09ba0d247a73fc36d46a
Daniel Stoa <daniel.stoa@kartverket.no>
2022-01-03 12:04:20 +0100

    Bruke caret left som lukkeikon
    Former-commit-id: 62fc7fb4ea92f0870fc79a11feb9b6e984b72cec

--- src/components/Sidebar/SidebarPanelTitle.tsx
+++ src/components/Sidebar/SidebarPanelTitle.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Button from "components/Button";
-import { ReactComponent as CaretLeft } from "icons/cog.svg";
+import { ReactComponent as CaretLeft } from "icons/caretleft.svg";
 
 type Props = {
   closePanel: () => void;
