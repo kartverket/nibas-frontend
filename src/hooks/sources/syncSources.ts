91620a191b569e2bc9969ae44f9894feb672879a
Daniel Stoa <daniel.stoa@kartverket.no>
2022-01-04 12:17:16 +0100

    NIBAS-240: Lag tilegrid for hver WMS source
    Denne gjør at vi kan hente større tiles, som gir færre requests til server
    
    
    Former-commit-id: 17dba9ac962f759c4c594fa4ea30bdec5a0a79f2

--- src/hooks/sources/syncSources.ts
+++ src/hooks/sources/syncSources.ts
@@ -1,19 +1,46 @@
 import TileWMS from "ol/source/TileWMS";
+import { createXYZ } from "ol/tilegrid";
 import { SyncSourceId, SyncSources } from "./types";
+
+const getWMSTileGrid = () => {
+  const tileSize = 1056;
+  // http://epsg.io/25833
+  // extent for EPSG:25833
+  const extent = [-2465144.8, 4102893.55, 776625.76, 9408555.22];
+  const tileGrid = createXYZ({
+    extent,
+    tileSize,
+  });
+
+  return tileGrid;
+};
 
 const administrativeEnheterSource = new TileWMS({
   url: "https://wms.geonorge.no/skwms1/wms.adm_enheter2?service=wms",
-  params: { LAYERS: "adm_enheter_V2_WMS", CRS: "EPSG:25833" },
+  params: { LAYERS: "adm_enheter_V2_WMS", CRS: "EPSG:25833", TILED: true },
+  tileGrid: getWMSTileGrid(),
 });
 
 const stedsnavnSource = new TileWMS({
   url: "http://openwms.statkart.no/skwms1/wms.stedsnavnenkel?version=1.3.0&service=wms",
-  params: { LAYERS: "stedsnavnenkel", CRS: "EPSG:25833", format: "image/png" },
+  params: {
+    LAYERS: "stedsnavnenkel",
+    CRS: "EPSG:25833",
+    format: "image/png",
+    TILED: true,
+  },
+  tileGrid: getWMSTileGrid(),
 });
 
 const topografiskNorgeskartSource = new TileWMS({
   url: "https://openwms.statkart.no/skwms1/wms.topo4?service=wms",
-  params: { LAYERS: "topo4_WMS", CRS: "EPSG:25833", format: "image/png" },
+  params: {
+    LAYERS: "topo4_WMS",
+    CRS: "EPSG:25833",
+    format: "image/png",
+    TILED: true,
+  },
+  tileGrid: getWMSTileGrid(),
 });
 
 export const syncSources: SyncSources = {
