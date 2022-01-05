9fff39d2d820717294ccbac0c3fb7c5bc3f58fc7
Daniel Stoa <daniel.stoa@kartverket.no>
2022-01-05 13:58:30 +0100

    NIBAS-224: Oppdatere en testtittel til noe som kanskje gir mer mening
    Former-commit-id: dfa71003a91964082741cb56950a6eb6d8cf30ea

--- src/components/GrenserDrillDown/KommuneList/KommuneList.test.tsx
+++ src/components/GrenserDrillDown/KommuneList/KommuneList.test.tsx
@@ -3,22 +3,13 @@ import KommuneList from "./KommuneList";
 
 const defaultProps: React.ComponentProps<typeof KommuneList> = {
   canSelect: true,
-  kommuneValues: {
-    fylke1: {
-      editing: false,
-      visible: false,
-    },
-    fylke2: {
-      editing: false,
-      visible: false,
-    },
-  },
-  fylke: { id: 2, navn: [{ navn: "Fylke2", spraak: "nor" }], type: "FYLKE" },
+  kommuneValues: {},
+  fylke: { id: 2, navn: [{ navn: "Fylke", spraak: "nor" }], type: "FYLKE" },
   setKommuneValue: jest.fn(),
 };
 
 describe("KommuneList", () => {
-  it("should render two kommuner based on fylke", async () => {
+  it("should render two kommuner from API request", async () => {
     render(<KommuneList {...defaultProps} />);
 
     expect(await screen.findByText("Malvik")).toBeInTheDocument();
