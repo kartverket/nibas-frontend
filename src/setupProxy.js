94325b941aa36993cfadd4e2252b40fcab9b8e8a
Daniel Stoa <daniel.stoa@kartverket.no>
2022-01-14 15:47:31 +0100

    NIBAS-248: Legge til setupProxy
    Former-commit-id: 762d29e74aef360b2666866274980d9b1430d9ab

---
+++ src/setupProxy.js
@@ -0,0 +1,54 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const matWfsUsername = process.env.REACT_APP_MATRIKKELWFS_USERNAME;
+const matWfsPassword = process.env.REACT_APP_MATRIKKELWFS_PASSWORD;
+
+const baatUsername = process.env.REACT_APP_BAAT_USERNAME;
+const baatPassword = process.env.REACT_APP_BAAT_PASSWORD;
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/v1", {
+      target: "http://localhost:8080",
+      changeOrigin: true,
+    })
+  );
+
+  app.use(
+    createProxyMiddleware("/geoservergeo/wfs/MATRIKKEL", {
+      target: "https://prodtest.matrikkel.no",
+      changeOrigin: true,
+      headers: {
+        Authorization:
+          "Basic " +
+          new Buffer(matWfsUsername + ":" + matWfsPassword).toString("base64"),
+      },
+    })
+  );
+
+  app.use(
+    createProxyMiddleware("/skbaatts/req", {
+      target: "https://baat.geonorge.no",
+      changeOrigin: true,
+      pathRewrite: (path) => {
+        let tjenesteId = "";
+
+        if (path.includes("tjenesteid=wms.ecc_enc")) {
+          tjenesteId = "wms.ecc_enc";
+        } else if (path.includes("tjenesteid=wms.nib")) {
+          tjenesteId = "wms.nib";
+        }
+
+        return `/skbaatts/req?tjenesteid=${tjenesteId}&brukerid=${baatUsername}&passord=${baatPassword}&retformat=s`;
+      },
+    })
+  );
+
+  app.use(
+    createProxyMiddleware(["/skwms1/wms.nib", "/skwms1/wms.ecc_enc"], {
+      target: "https://wms.geonorge.no",
+      changeOrigin: true,
+    })
+  );
+};
