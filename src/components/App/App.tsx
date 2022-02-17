098f55ab4745aeca9b0007dc67823d8007bbb5b3
Anders Østen <ostean@kartverket.no>
2022-02-17 10:59:02 +0100

    NIBAS-154: Lagt inn superenkel autentisering over hoved-applikasjonen.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,65 @@
+import {
+  ConfigureAuthFlowProps,
+  useConfigureAuthFlow,
+} from "@kartverket/frontend-aut-lib";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Providers from "./Providers";
+import LogoutButton from "components/Authentication/LogoutButton";
+import ProtectedTokenInfo from "components/Authentication/ProtectedTokenInfo";
+import StatusBar from "components/Authentication/StatusBar";
 import PageLayout from "components/PageLayout";
 
+/**
+ * Definerer 3 verdier i konfigurasjonen. Disse brukes av biblioteket forskjellige steder i flyten.
+ */
+const authFlowProps: ConfigureAuthFlowProps = {
+  systemId: "nibas",
+  fallbackUrl: "/",
+  afterUserLogoutRedirect: "/",
+};
+
+/**
+ * Bruker hook useConfigureAuthFlow for å lagre verdiene angitt over i ConfigureAuthFlowProps.
+ * Denne returnerer 2 <Route>-objekter som brukes i routingen (se lenger ned) for å kunne initialisere logikk knyttet
+ * til autentiseringsflyten, samt utloggingsflyt. Siden disse returneres i en array, kan de brukes direkte i routing-JSX.
+ */
+
 const App = () => {
+  const [redirectAfterLogon, redirectAfterLogout]: JSX.Element[] =
+    useConfigureAuthFlow(authFlowProps);
   return (
-    <Providers>
-      <PageLayout />
-    </Providers>
+    <Router>
+      <StatusBar />
+      <LogoutButton />
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home sweet home</Link>
+          </li>
+          <li>
+            <Link to="/protectedTokenInfo">
+              Link til info om token (autentisering påkrevd)
+            </Link>
+          </li>
+          <li>
+            <Link to="/app">Link til app</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        {redirectAfterLogon}
+        {redirectAfterLogout}
+        <Route path="/protectedTokenInfo" element={<ProtectedTokenInfo />} />
+        <Route
+          path="/app"
+          element={
+            <Providers>
+              <PageLayout />
+            </Providers>
+          }
+        ></Route>
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/components/Authentication/LogoutButton.tsx
@@ -0,0 +1,17 @@
+import { useAuthenticationFlow } from "@kartverket/frontend-aut-lib";
+
+/**
+ * Eksempel som rendrer en login eller logout-knapp basert på om man er autentisert.
+ * Henter ut isAuthenticated-funksjonen, samt login og logout-funksjonene fra useAuthenticationFlow-hook.
+ */
+export default function LogoutButton() {
+  const { isAuthenticatedFunc, handleAuthenticateFunc, handleLogoutFunc } =
+    useAuthenticationFlow();
+
+  const loginButton = (
+    <button onClick={() => handleAuthenticateFunc("/")}>Login</button>
+  );
+  const logoutButton = <button onClick={handleLogoutFunc}>Logout</button>;
+
+  return isAuthenticatedFunc() ? logoutButton : loginButton;
+}
