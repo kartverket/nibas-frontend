599a7aef73d7b88554de911cb248926bb36b99b5
Anders Østen <ostean@kartverket.no>
2022-02-17 15:12:48 +0100

    NIBAS-154: Fikset feilende tester

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -5,7 +5,7 @@ describe("App", () => {
   it("should render without exploding", () => {
     render(<App />);
 
-    const nibasTitle = screen.getByText("Nasjonal inndelingsbase");
+    const nibasTitle = screen.getByText("Home sweet home");
 
     expect(nibasTitle).toBeInTheDocument();
   });
