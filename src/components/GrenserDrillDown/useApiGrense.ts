4485d093cb96b7f6aabffc0b316b84a1132441ec
Daniel Stoa <daniel.stoa@kartverket.no>
2022-02-22 16:13:00 +0100

    NIBAS-288: Gjør grenser immutable så de ikke oppdateres

--- src/components/GrenserDrillDown/useApiGrense.ts
+++ src/components/GrenserDrillDown/useApiGrense.ts
@@ -1,20 +1,15 @@
 import { useCallback, useMemo, useState } from "react";
 import { Feature } from "ol";
 import Geometry from "ol/geom/Geometry";
-import useSWR from "swr";
+import useSWRImmutable from "swr/immutable";
 import { geoJsonToSource } from "utils/map/geoJson";
 import { fetcher } from "utils/swr";
 
 const useApiGrense = (featuresUrl: string) => {
   const [shouldFetch, setShouldFetch] = useState(false);
-  const { data: geoJson } = useSWR<Feature<Geometry>>(
+  const { data: geoJson } = useSWRImmutable<Feature<Geometry>>(
     shouldFetch ? featuresUrl : null,
-    fetcher,
-    {
-      // vi ønsker ikke å refreshe dataene uten er refresh av siden?
-      revalidateOnFocus: false,
-      revalidateOnReconnect: false,
-    }
+    fetcher
   );
 
   const features = useMemo(() => {
