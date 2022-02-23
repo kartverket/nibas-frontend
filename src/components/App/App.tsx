0f4cab8ed1ae86751eae6e907404080ccdaaa7b6
Anders Østen <a.osten@gmail.com>
2022-02-23 09:08:30 +0100

    Nibas-154: Autentisering mot id-porten
    Innhold:
    - Enkelt oppsett av en innlogging mot id-porten
    - Oppsett av workflow for å hente autentiseringsbibliotek

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,38 @@
+import {
+  ConfigureAuthFlowProps,
+  useConfigureAuthFlow,
+} from "@kartverket/frontend-aut-lib";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
 import Providers from "./Providers";
 import PageLayout from "components/PageLayout";
 
+/**
+ * Definerer 3 verdier i konfigurasjonen. Disse brukes av biblioteket forskjellige steder i flyten.
+ */
+const authFlowProps: ConfigureAuthFlowProps = {
+  systemId: "nibas",
+  fallbackUrl: "/",
+  afterUserLogoutRedirect: "/",
+};
+
 const App = () => {
+  /**
+   * Bruker hook useConfigureAuthFlow for å lagre verdiene angitt over i ConfigureAuthFlowProps.
+   * Denne returnerer 2 <Route>-objekter som brukes i routingen (se lenger ned) for å kunne initialisere logikk knyttet
+   * til autentiseringsflyten, samt utloggingsflyt.
+   */
+  const [redirectAfterLogon, redirectAfterLogout]: JSX.Element[] =
+    useConfigureAuthFlow(authFlowProps);
   return (
-    <Providers>
-      <PageLayout />
-    </Providers>
+    <Router>
+      <Providers>
+        <PageLayout />
+      </Providers>
+      <Routes>
+        {redirectAfterLogon}
+        {redirectAfterLogout}
+      </Routes>
+    </Router>
   );
 };
 
