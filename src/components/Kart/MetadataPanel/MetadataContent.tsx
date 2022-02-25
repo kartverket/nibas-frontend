7d3dc2411ae02c7d85e1d38adb25e757df786f32
Daniel Stoa <daniel.stoa@kartverket.no>
2022-02-25 12:39:58 +0100

    NIBAS-127: Bruke metadata fra grenser fra nytt API

--- src/components/Kart/MetadataPanel/MetadataContent.tsx
+++ src/components/Kart/MetadataPanel/MetadataContent.tsx
@@ -1,80 +1,61 @@
 import { Feature } from "ol";
 import Geometry from "ol/geom/Geometry";
 import styled from "styled-components";
-import useMetadataFromFeature, {
-  KontekstType,
-  ResponseItem,
-} from "./useMetadataFromFeature";
 import Checkbox from "components/Checkbox";
 import Input from "components/Input";
 import Select from "components/Select";
-import { Fylke } from "types/api";
 
 type Props = {
   feature: Feature<Geometry>;
 };
 
-const isFylke = (
-  item: ResponseItem,
-  kontekstType: KontekstType
-): item is Fylke => {
-  return kontekstType === "FYLKE";
-};
-
 const MetadataContent = ({ feature }: Props) => {
-  const { item, kontekstType } = useMetadataFromFeature(feature);
+  console.log(feature.getProperties());
+  const { type, metadata } = feature.getProperties();
 
-  // console.log(feature.getProperties());
-
-  if (!item || !kontekstType) return null;
-
-  if (isFylke(item, kontekstType)) {
-    return (
-      <Container>
-        <Part>
-          <label>
-            Grensetype
-            <Select disabled>
-              <option>Testing</option>
-            </Select>
-          </label>
-          <div>
-            <p>Omtvisted</p>
-            <Checkbox type="radio" label="Ja" disabled defaultChecked={true} />
-            <Checkbox type="radio" label="Nei" disabled />
-          </div>
-          <BlockLabel>
-            Gyldig fra
-            <Input disabled defaultValue="01.04.1984" />
-          </BlockLabel>
-          <BlockLabel>
-            Gyldig til
-            <Input disabled defaultValue="01.12.2099" />
-          </BlockLabel>
-        </Part>
-        <Part>
-          <label>
-            Nøyaktighetsklasse
-            <Select disabled>
-              <option>God</option>
-            </Select>
-          </label>
-          <label>
-            Målemetode
-            <Select disabled>
-              <option>Input</option>
-            </Select>
-          </label>
-        </Part>
-        <Part>
-          <p>Oppdateringsdato: {"12.03.1997"}</p>
-          <p>Datafangsdato: {"01.04.1979"}</p>
-        </Part>
-      </Container>
-    );
-  }
-
-  return <div></div>;
+  return (
+    <Container>
+      <Part>
+        <label>
+          Grensetype
+          <Select disabled>
+            <option>{type}</option>
+          </Select>
+        </label>
+        <div>
+          <p>Omtvisted</p>
+          <Checkbox type="radio" label="Ja" disabled defaultChecked={true} />
+          <Checkbox type="radio" label="Nei" disabled />
+        </div>
+        <BlockLabel>
+          Gyldig fra
+          <Input disabled defaultValue={metadata?.gyldigFra ?? "---"} />
+        </BlockLabel>
+        <BlockLabel>
+          Gyldig til
+          <Input disabled defaultValue={metadata?.gyldigTil ?? "---"} />
+        </BlockLabel>
+      </Part>
+      <Part>
+        <label>
+          Nøyaktighetsklasse
+          <Select disabled>
+            <option>God</option>
+          </Select>
+        </label>
+        <label>
+          Målemetode
+          <Select disabled>
+            <option>Input</option>
+          </Select>
+        </label>
+      </Part>
+      <Part>
+        <p>Oppdateringsdato: {metadata?.oppdateringsdato ?? "---"}</p>
+        <p>Datafangsdato: {metadata?.datafangstdato ?? "---"}</p>
+      </Part>
+    </Container>
+  );
 };
 
 const Container = styled.div`
