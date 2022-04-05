c18962eb1359333810bd7d1c57a906e22a1d9a9b
Daniel Stoa <daniel.stoa@kartverket.no>
2022-04-05 10:38:15 +0200

    NIBAS-370: Sette api grenser til de i kartet hvis de finnes

--- src/components/GrenserDrillDown/ApiGrense/useApiGrense.ts
+++ src/components/GrenserDrillDown/ApiGrense/useApiGrense.ts
@@ -1,16 +1,16 @@
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useAuthenticationFlow } from "@kartverket/frontend-aut-lib";
-import { Feature } from "ol";
-import Geometry from "ol/geom/Geometry";
+import { GeoJSONFeatureCollection } from "ol/format/GeoJSON";
 import useSWRImmutable from "swr/immutable";
-import { geoJsonToSource } from "utils/map/geoJson";
+import { featuresToGeoJson, geoJsonToSource } from "utils/map/geoJson";
+import { getLayerById } from "utils/map/layers";
 import { fetcherWithToken } from "utils/swr";
 
 const useApiGrense = (featuresUrl: string, shouldFetchInitially = false) => {
   const [shouldFetch, setShouldFetch] = useState(shouldFetchInitially);
   const { tokenHolderFunc } = useAuthenticationFlow();
 
-  const { data: geoJson, mutate } = useSWRImmutable<Feature<Geometry>>(
+  const { data: geoJson, mutate } = useSWRImmutable<GeoJSONFeatureCollection>(
     shouldFetch ? [featuresUrl, tokenHolderFunc()?.token] : null,
     fetcherWithToken
   );
@@ -20,6 +20,22 @@ const useApiGrense = (featuresUrl: string, shouldFetchInitially = false) => {
 
     return geoJsonToSource(geoJson).getFeatures();
   }, [geoJson]);
+
+  useEffect(() => {
+    if (!features || !shouldFetch) return;
+
+    // sjekk om features allerede ligger i kartet
+    // hvis featurene er annerledes enn vanlig, så ligger de endrede featurene i edit-laget
+    const allFeaturesInMap = getLayerById("edit").getSource().getFeatures();
+
+    const updatedFeatures = allFeaturesInMap.filter((feature) =>
+      features.some((apiFeature) => apiFeature.getId() === feature.getId())
+    );
+
+    if (updatedFeatures.length > 0) {
+      mutate(featuresToGeoJson(updatedFeatures));
+    }
+  }, [features, mutate, shouldFetch]);
 
   const fetchFeatures = useCallback(() => {
     setShouldFetch(true);
