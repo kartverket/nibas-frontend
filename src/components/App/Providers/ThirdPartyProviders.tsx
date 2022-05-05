5e1c28fe0510705b3e2afbdd778fb8a66e4f0b85
Daniel Stoa <daniel.stoa@kartverket.no>
2022-05-05 14:52:13 +0200

    NIBAS-286: Splitt ut tredjeparts providers i egen komponent

---
+++ src/components/App/Providers/ThirdPartyProviders.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "style/theme";
+
+const ThirdPartyProviders: FC = ({ children }) => {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+    </DndProvider>
+  );
+};
+
+export default ThirdPartyProviders;
