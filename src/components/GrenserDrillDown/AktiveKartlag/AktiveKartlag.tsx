33c70eb96dbf2815f98913bc007160c63ae00479
Daniel Stoa <daniel.stoa@kartverket.no>
2022-06-17 09:18:29 +0200

    NIBAS-467: Fjerne grunnkretser fra aktive kartlag
    Vi skal kanskje ikke ha komponenten i det hele tatt, så bruker ikke tid på å implementere custom funksjonalitet

--- src/components/GrenserDrillDown/AktiveKartlag/AktiveKartlag.tsx
+++ src/components/GrenserDrillDown/AktiveKartlag/AktiveKartlag.tsx
@@ -1,7 +1,6 @@
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 import FylkeList from "../Fylkesgrenser/FylkeList";
-import GrunnkretsList from "../Grunnkretser/GrunnkretsList";
 import KommuneList from "../Kommunegrenser/KommuneList";
 import MainLayer from "components/Bakgrunnskart/MainLayer";
 import { useBakgrunnskart } from "contexts/BakgrunnskartContext";
@@ -23,7 +22,6 @@ const AktiveKartlag = () => {
       {fylker?.map((fylke) => (
         <KommuneList key={fylke.id} onlyDisplayEditing fylke={fylke} />
       ))}
-      {/* <GrunnkretsList onlyDisplayEditing /> */}
 
       <ActiveBackgroundLayers>
         {t("Aktive bakgrunnskart")}
