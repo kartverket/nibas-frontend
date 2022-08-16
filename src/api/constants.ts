bc2030b9a4b5a422c8757950711f3bb43c74ce6b
Daniel Stoa <daniel.stoa@kartverket.no>
2022-08-16 15:56:42 +0200

    NIBAS-510: Legge til global SWR provider

---
+++ src/api/constants.ts
@@ -0,0 +1,8 @@
+import { EditingType } from "contexts/EditGrenserContext";
+
+export const endpointByEditingType: Record<EditingType, string> = {
+  fylke: "fylker",
+  grunnkrets: "grunnkretser",
+  kommune: "kommuner",
+  nasjon: "nasjoner",
+};
--- src/components/App/Providers/ThirdPartyProviders.tsx
+++ src/components/App/Providers/ThirdPartyProviders.tsx
@@ -2,12 +2,15 @@ import { FC } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { ThemeProvider } from "styled-components";
+import { SWRConfig } from "swr";
 import { defaultTheme } from "style/theme";
 
 const ThirdPartyProviders: FC = ({ children }) => {
   return (
     <DndProvider backend={HTML5Backend}>
-      <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+      <ThemeProvider theme={defaultTheme}>
+        <SWRConfig>{children}</SWRConfig>
+      </ThemeProvider>
     </DndProvider>
   );
 };
