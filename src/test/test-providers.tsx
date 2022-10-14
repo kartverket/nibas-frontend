534dff2c4158e642f33fe0ea33a311507a4aeef2
Daniel Stoa <daniel.stoa@kartverket.no>
2022-10-14 09:10:01 +0200

    TS-560: Skru av BakgrunnskartProvider by default

--- src/test/test-providers.tsx
+++ src/test/test-providers.tsx
@@ -51,38 +51,49 @@ const contextMap = {
 };
 
 export type TestProviderValues = {
-  BakgrunnskartProvider?: BakgrunnskartContextValue;
-  EditGrenserProvider?: EditGrenserContextValue;
-  MetadataPanelProvider?: MetadataPanelContextValue;
-  SidebarPanelProvider?: SidebarPanelContextValue;
-  ToolbarProvider?: ToolbarContextValue;
-  UtkastProvider?: UtkastContextValue;
+  BakgrunnskartProvider?: BakgrunnskartContextValue | boolean;
+  EditGrenserProvider?: EditGrenserContextValue | boolean;
+  MetadataPanelProvider?: MetadataPanelContextValue | boolean;
+  SidebarPanelProvider?: SidebarPanelContextValue | boolean;
+  ToolbarProvider?: ToolbarContextValue | boolean;
+  UtkastProvider?: UtkastContextValue | boolean;
 };
 
 type ProviderName = keyof typeof defaultProviderMap;
 
 export const renderWithProviders = (
   ui: ReactNode,
-  providerValues: TestProviderValues = {}
+  providerValues: TestProviderValues = {
+    BakgrunnskartProvider: false,
+  }
 ) => {
+  // BakgrunnskartProvider lager kvalme i testene våre, så den er default av
+  if (providerValues.BakgrunnskartProvider === undefined) {
+    providerValues.BakgrunnskartProvider = false;
+  }
+
   return (
     <ThirdPartyProviders>
       <BrowserRouter>
         {Object.keys(defaultProviderMap).reduceRight((acc, name) => {
           const providerName = name as ProviderName;
+          const value = providerValues[providerName];
 
-          // bruk override dersom det er sendt med
-          if (providerValues[providerName]) {
-            const Provider = contextMap[providerName];
-            const value = providerValues[providerName];
-
-            // TS skjønner ikke at value tilhører Provider her, så vi kjører en liten 🤠
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            return <Provider value={value as any}>{acc}</Provider>;
+          if (value === true || value === undefined) {
+            // hvis ikke satt eller eksplisitt satt til true, wrap i default provider
+            const Provider = defaultProviderMap[providerName];
+            return <Provider>{acc}</Provider>;
+          } else if (value === false) {
+            // hvis satt til false, ikke wrap i noen provider
+            return acc;
           }
 
-          const Provider = defaultProviderMap[providerName];
-          return <Provider>{acc}</Provider>;
+          // ellers bruk override
+          const Provider = contextMap[providerName];
+
+          // TS skjønner ikke at value tilhører Provider her, så vi kjører en liten 🤠
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          return <Provider value={value as any}>{acc}</Provider>;
         }, ui)}
       </BrowserRouter>
     </ThirdPartyProviders>
