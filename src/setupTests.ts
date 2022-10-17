9ec518dfd7dc84e7012df535e4d7ba4d20b889ba
Daniel Stoa <danielstoa@outlook.com>
2022-10-17 11:07:00 +0200

    TS-564: Mocke i18n i tester

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -17,3 +17,18 @@ beforeAll(() => {
 afterEach(() => server.resetHandlers());
 // Clean up after the tests are finished.
 afterAll(() => server.close());
+
+jest.mock("react-i18next", () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+      i18n: {
+        changeLanguage: () =>
+          new Promise(() => {
+            // do nothing
+          }),
+      },
+    };
+  },
+}));
