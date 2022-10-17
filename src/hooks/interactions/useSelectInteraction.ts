cf819e9bdbab59a054ae8c07e775f80ac881f7c4
Daniel Stoa <danielstoa@outlook.com>
2022-10-17 15:37:24 +0200

    TS-490: Sette riktig farge på feature når metadatapanel lukkes

--- src/hooks/interactions/useSelectInteraction.ts
+++ src/hooks/interactions/useSelectInteraction.ts
@@ -5,7 +5,10 @@ import LineString from "ol/geom/LineString";
 import { Select } from "ol/interaction";
 import { map, overlayPopup } from "components/Kart/constants";
 import { useMetadataPanel } from "contexts/MetadataPanelContext";
-import { selectStyles } from "utils/map/layerStyles";
+import { dirtyStyles, selectStyles } from "utils/map/layerStyles";
+import Style from "ol/style/Style";
+import { getFeatureId } from "utils/map/source";
+import { useToolbar } from "contexts/ToolbarContext";
 
 const getOverlayPosition = (selectedFeature: Feature<LineString>) => {
   const coordinates = selectedFeature.getGeometry()?.getCoordinates() ?? [];
@@ -18,11 +21,12 @@ const getOverlayPosition = (selectedFeature: Feature<LineString>) => {
 };
 
 const useSelectInteraction = () => {
+  const { dirtyFeatureIds } = useToolbar();
   const [features, setFeatures] = useState<Feature<Geometry>[]>([]);
   const { openPanel, closePanel } = useMetadataPanel();
 
   useEffect(() => {
-    const select = new Select({ hitTolerance: 5, style: selectStyles });
+    const select = new Select({ hitTolerance: 5, style: null });
 
     select.on("select", () => {
       setFeatures(select.getFeatures().getArray().slice());
@@ -51,6 +55,27 @@ const useSelectInteraction = () => {
       overlayPopup.setPosition(undefined);
     }
   }, [features, openPanel, closePanel]);
+
+  useEffect(() => {
+    const previousStylesByFeatureId: Record<string, Style[]> = {};
+
+    features.forEach((feature) => {
+      const featureId = getFeatureId(feature);
+      previousStylesByFeatureId[featureId] = feature.getStyle() as Style[];
+      feature.setStyle(selectStyles);
+    });
+
+    return () => {
+      features.forEach((feature) => {
+        const featureId = getFeatureId(feature);
+        if (dirtyFeatureIds.includes(featureId)) {
+          feature.setStyle(dirtyStyles);
+        } else {
+          feature.setStyle(previousStylesByFeatureId[featureId]);
+        }
+      });
+    };
+  }, [features, dirtyFeatureIds]);
 
   return features;
 };
