e1b9735ce6f32e6e74fd488a11f7332dccfa5ae9
Daniel Stoa <danielstoa@outlook.com>
2022-11-03 13:28:27 +0100

    TS-576: Legge til øye på fylkesgrenser

--- src/components/GrenserDrillDown/Fylkesgrenser/Fylkesgrenser.tsx
+++ src/components/GrenserDrillDown/Fylkesgrenser/Fylkesgrenser.tsx
@@ -1,58 +1,93 @@
-import { LinkButton } from "components/form/Button";
+import Button, { LinkButton } from "components/form/Button";
+import Icon from "components/Icon";
 import Loader from "components/Loader";
 import { useEditGrense } from "contexts/EditGrenserContext/useEditGrense";
 import useFylkesgrenser from "hooks/inndelinger/useFylkesgrenser";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
-import ListItemAccordion from "../ListItemAccordion";
 import FylkeList from "./FylkeList";
+import useVisibility from "hooks/useVisibility";
 
 const Fylkesgrenser = () => {
   const { t } = useTranslation();
   const [shouldFetch, setShouldFetch] = useState(false);
   const { fylkesgrenser, isFetching } = useFylkesgrenser(shouldFetch);
-  const { value, toggleEditing } = useEditGrense(
+  const { value, toggleEditing, toggleVisible } = useEditGrense(
     "fylke",
     "fylke",
     fylkesgrenser
   );
   console.log(fylkesgrenser);
 
+  const accordion = useVisibility();
+
   useEffect(() => {
-    if (value.editing) {
+    if (value.editing || value.visible) {
       setShouldFetch(true);
     }
-  }, [value.editing]);
+  }, [value]);
 
   const editFylkesgrenser = () => {
     toggleEditing();
   };
 
   return (
-    <ListItemAccordion
-      subButton={
-        <LinkButton onClick={editFylkesgrenser}>
-          {value.editing ? "Stopp redigering" : "Rediger fylkesgrenser"}
-        </LinkButton>
-      }
-      title={
-        <TitleWithEditButton>
-          {t("inndelinger.Fylkesgrenser")}
+    <ListItem>
+      <Header>
+        <VisibilityButton onClick={toggleVisible}>
+          {value.visible ? (
+            <Icon icon="visibility" />
+          ) : (
+            <Icon icon="visibility_off" />
+          )}
+        </VisibilityButton>
+        <TextContent>
+          <Button variant="unstyled" onClick={accordion.toggle}>
+            {t("inndelinger.Fylkesgrenser")}
+          </Button>
+          <div>
+            <LinkButton onClick={editFylkesgrenser}>
+              {value.editing ? "Stopp redigering" : "Rediger fylkesgrenser"}
+            </LinkButton>
+          </div>
+        </TextContent>
+        {isFetching && <Loader />}
+        <Button
+          variant="unstyled"
+          onClick={accordion.toggle}
+          icon={
+            accordion.isVisible ? (
+              <Icon icon="expand_less" aria-label="Lukk" />
+            ) : (
+              <Icon icon="expand_more" aria-label="Åpne" />
+            )
+          }
+        />
+      </Header>
 
-          {isFetching && <Loader />}
-        </TitleWithEditButton>
-      }
-    >
-      <FylkeList />
-    </ListItemAccordion>
+      {accordion.isVisible && <FylkeList />}
+    </ListItem>
   );
 };
 
-const TitleWithEditButton = styled.div`
+const ListItem = styled.li``;
+
+const TextContent = styled.div`
+  flex: 1;
   display: flex;
   flex-direction: column;
-  gap: 8px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const VisibilityButton = styled(Button).attrs(() => ({
+  variant: "unstyled",
+}))`
+  margin-right: 16px;
 `;
 
 export default Fylkesgrenser;
