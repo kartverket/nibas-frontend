df37172d8a94dc8f9072f6dc73b578ad90fa5d73
Sofie Tjønneland Urhaug <39772759+sofieurhaug@users.noreply.github.com>
2023-01-11 08:47:10 +0100

    🔥 Fjerne inndelinger uten innhold

--- src/components/GrenserDrillDown/GrenserDrillDown.tsx
+++ src/components/GrenserDrillDown/GrenserDrillDown.tsx
@@ -1,16 +1,9 @@
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
-import Delomrader from "./Delomrader";
 import Fylkesgrenser from "./Fylkesgrenser";
-import GestligeInndelinger from "./GestligeInndelinger";
 import Grunnkretser from "./Grunnkretser";
 import Kommunegrenser from "./Kommunegrenser";
-import MaritimeGrenser from "./MaritimeGrenser";
-import Postnummeromraader from "./Postnummeromraader";
-import Riksgrenser from "./Riksgrenser";
-import Skolekretser from "./Skolekretser";
 import Stemmekretser from "./Stemmekretser";
-import Svalbardomradet from "./Svalbardomraadet";
 import Accordion from "components/Accordion";
 import { SidebarPanel } from "components/Sidebar/SidebarPanel";
 import SidebarPanelTitle from "components/Sidebar/SidebarPanelTitle";
@@ -31,17 +24,10 @@ const GrenserDrillDown = () => {
         title={t("sidebar.Inndelinger")}
       />
       <UnstyledList>
-        <Riksgrenser />
         <Fylkesgrenser />
         <Kommunegrenser />
         <Stemmekretser />
-        <Skolekretser />
         <Grunnkretser />
-        <Delomrader />
-        <Postnummeromraader />
-        <GestligeInndelinger />
-        <MaritimeGrenser />
-        <Svalbardomradet />
       </UnstyledList>
     </Panel>
   );
