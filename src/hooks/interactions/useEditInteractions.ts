9de90c176cfc4ac0669e8ff1e409d6961a3cb808
Anders Larsen <andelar96@gmail.com>
2023-02-08 08:53:25 +0100

    Midlertidig fiks til history

--- src/hooks/interactions/useEditInteractions.ts
+++ src/hooks/interactions/useEditInteractions.ts
@@ -140,6 +140,11 @@ const useEditInteractions = () => {
                     from: originalCoordinates,
                     to: featureInfo.coordinates,
                   },
+                ],
+              });
+              addEntry({
+                type: "grense",
+                changes: [
                   {
                     id: clonedFeatureInfo.featureId as string,
                     from: [],
