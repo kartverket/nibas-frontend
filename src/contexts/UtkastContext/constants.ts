c7b685cf1de11aac1d6e9a1d00410fc2725e6179
Jorunn Toft <tofjor@kartverket.no>
2023-02-09 10:26:16 +0100

    TS-902: Flyttet typen Retting til slutt i lista

--- src/contexts/UtkastContext/constants.ts
+++ src/contexts/UtkastContext/constants.ts
@@ -1,9 +1,9 @@
 export const translateKeysByEndringsType: Record<string, string> = {
   "Vedtatt grensejustering": "utkast.Vedtatt grensejustering",
   "Vedtatt sammenslåing": "utkast.Vedtatt sammenslåing",
-  Retting: "utkast.Retting",
   "Vedtatt deling": "utkast.Vedtatt deling",
   Fastsetting: "utkast.Fastsetting",
   Navneendring: "utkast.Navneendring",
   Nummerendring: "utkast.Nummerendring",
+  Retting: "utkast.Retting",
 };
