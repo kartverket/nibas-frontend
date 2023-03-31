cc725c38f660b40966f8716c8bfed3c685dccf27
Steffen Hageland <steffen.hageland@bekk.no>
2023-03-31 13:05:15 +0200

    TS-969 Vise det nye navnet i endringsloggen

--- src/components/Endringslogg/EndringsloggGrunnkretsendringer.tsx
+++ src/components/Endringslogg/EndringsloggGrunnkretsendringer.tsx
@@ -80,13 +80,16 @@ const GrunnkretsMetadataEndringer = ({
   metadataendring,
 }: GrunnkretsMetadataEndringerProps) => {
   const { t } = useTranslation();
+  const navn = metadataendring.navn ?? metadataendring.kretsEndret.navn;
+  const nummer =
+    metadataendring.grunnkretsnummer ??
+    metadataendring.kretsEndret.grunnkretsnummer;
 
   return (
     <EndringSection>
       <Seksjonsoverskrift>
         <span>
-          {metadataendring.kretsEndret.grunnkretsnummer}{" "}
-          {metadataendring.kretsEndret.navn}
+          {navn} {nummer}
         </span>
         <EndringstypeTag>{t("utkast.endringslogg.metadata")}</EndringstypeTag>
       </Seksjonsoverskrift>
