4282697c17abbafe74d725c1b941beb9b0ce14bd
Steffen Hageland <steffen.hageland@kartverket.no>
2023-06-05 10:04:25 +0200

    Sortere utkast basert på opprettet-dato

--- src/types/api-gen.ts
+++ src/types/api-gen.ts
@@ -124,6 +124,10 @@ export interface paths {
     /** Henter stemmekrets med gitt id */
     get: operations["hentStemmekrets_1"];
   };
+  "/v1/ekstern/stemmekretser/{id}/grenser": {
+    /** Henter grensene til en stemmekrets med gitt id */
+    get: operations["hentGrenserForStemmekrets_1"];
+  };
   "/v1/ekstern/kommuner": {
     /** Henter alle kommuner i Nasjonal inndelingsbase. */
     get: operations["hentKommuner_1"];
@@ -143,6 +147,9 @@ export interface paths {
   "/v1/ekstern/grunnkretser": {
     /** Henter grunnkretser med gitte id-er */
     get: operations["hentGrunnkretserForIds"];
+  };
+  "/v1/ekstern/grunnkretser/{id}/grenser": {
+    get: operations["hentGrenserForGrunnkrets_1"];
   };
   "/v1/ekstern/fylker": {
     /** Henter alle fylker i Nasjonal inndelingsbase. */
@@ -171,6 +178,11 @@ export interface components {
        * @description Teknisk versjon for å støtte samhandling og redigering
        */
       version: number;
+      /**
+       * Format: int32
+       * @description Rekkefølge på navn
+       */
+      rekkefoelge?: number;
     };
     /** @description Spesfikk metadata for en administrativ grense (fylkes/kommunegrense). Beskrevet i SOSI-modellen her: https://objektkatalog.geonorge.no/Objekttype/Index/EAID_5142EDBA_4226_4ca3_924D_EECEFD216D1A */
     AdministrativGrenseMetadata: components["schemas"]["Metadata"] & {
@@ -178,6 +190,10 @@ export interface components {
       commonGrense?: components["schemas"]["CommonGrenseMetadata"];
       /** @description Henviser til fastsettings- eller lovinformasjon. */
       dokumentasjonsreferanser?: components["schemas"]["Dokref"][];
+    } & {
+      common: unknown;
+      commonGrense: unknown;
+      dokumentasjonsreferanser: unknown;
     };
     /** @description Spesifikk metadata for en AvtaltAvgrensningslinje. Beskrevet i SOSI-modellen her: https://objektkatalog.geonorge.no/Diagram/Index/EAID_EEECEE48_B3FA_4807_AAE4_B30B63BC28E1 */
     AvtaltAvgrensningslinjeMetadata: components["schemas"]["Metadata"] & {
@@ -185,6 +201,10 @@ export interface components {
       commonGrense?: components["schemas"]["CommonGrenseMetadata"];
       /** @description Henviser til fastsettings- eller lovinformasjon. */
       dokumentasjonsreferanser?: components["schemas"]["Dokref"][];
+    } & {
+      common: unknown;
+      commonGrense: unknown;
+      dokumentasjonsreferanser: unknown;
     };
     /** @description Felles metadata-egenskaper for grenser. */
     CommonGrenseMetadata: {
@@ -265,6 +285,8 @@ export interface components {
     /** @description Metadata for en flate (felles for nasjon, fylke, kommune og grunnkrets) */
     FlateMetadata: components["schemas"]["Metadata"] & {
       common?: components["schemas"]["CommonMetadata"];
+    } & {
+      common: unknown;
     };
     /** @description Endringer på fylke. */
     FylkeRequest: {
@@ -310,6 +332,10 @@ export interface components {
       commonGrense?: components["schemas"]["CommonGrenseMetadata"];
       /** @description Henviser til fastsettings- eller lovinformasjon. */
       dokumentasjonsreferanser?: components["schemas"]["Dokref"][];
+    } & {
+      common: unknown;
+      commonGrense: unknown;
+      dokumentasjonsreferanser: unknown;
     };
     /** @description Unik identifikasjon av et objekt */
     Identifikasjon: {
@@ -381,6 +407,9 @@ export interface components {
       type?: string;
       /** @description Liste av (x og y som liste). */
       coordinates?: number[][];
+    } & {
+      coordinates: unknown;
+      type: unknown;
     };
     Lokalid: {
       value: string;
@@ -416,6 +445,9 @@ export interface components {
       type?: string;
       /** @description Liste av polygonenes liste av LinearRings som igjen har liste av koordinater som er liste av x og y. */
       coordinates?: number[][][][];
+    } & {
+      coordinates: unknown;
+      type: unknown;
     };
     /** @description Endringer på nasjon. */
     NasjonRequest: {
@@ -465,6 +497,9 @@ export interface components {
       type?: string;
       /** @description X og y som liste. */
       coordinates?: number[];
+    } & {
+      coordinates: unknown;
+      type: unknown;
     };
     /** @description Beskrivelse av kvaliteten på stedfestingen */
     Posisjonskvalitet: {
@@ -481,6 +516,10 @@ export interface components {
       commonGrense?: components["schemas"]["CommonGrenseMetadata"];
       /** @description Henviser til fastsettings- eller lovinformasjon. */
       dokumentasjonsreferanser?: components["schemas"]["Dokref"][];
+    } & {
+      common: unknown;
+      commonGrense: unknown;
+      dokumentasjonsreferanser: unknown;
     };
     /** @description Holder på informasjon om endring av objektet. */
     Sporingsinformasjon: {
@@ -494,6 +533,9 @@ export interface components {
     StatistiskgrenseMetadata: components["schemas"]["Metadata"] & {
       common?: components["schemas"]["CommonMetadata"];
       commonGrense?: components["schemas"]["CommonGrenseMetadata"];
+    } & {
+      common: unknown;
+      commonGrense: unknown;
     };
     /** @description Representasjon av en stemmekrets */
     StemmekretsRequest: {
@@ -539,6 +581,88 @@ export interface components {
       commonGrense?: components["schemas"]["CommonGrenseMetadata"];
       /** @description Henviser til fastsettings- eller lovinformasjon. */
       dokumentasjonsreferanser?: components["schemas"]["Dokref"][];
+    } & {
+      common: unknown;
+      commonGrense: unknown;
+      dokumentasjonsreferanser: unknown;
+    };
+    ApiErrorResponse: {
+      /**
+       * @description HttpStatus for responsen.
+       * @enum {string}
+       */
+      httpStatus:
+        | "100 CONTINUE"
+        | "101 SWITCHING_PROTOCOLS"
+        | "102 PROCESSING"
+        | "103 EARLY_HINTS"
+        | "103 CHECKPOINT"
+        | "200 OK"
+        | "201 CREATED"
+        | "202 ACCEPTED"
+        | "203 NON_AUTHORITATIVE_INFORMATION"
+        | "204 NO_CONTENT"
+        | "205 RESET_CONTENT"
+        | "206 PARTIAL_CONTENT"
+        | "207 MULTI_STATUS"
+        | "208 ALREADY_REPORTED"
+        | "226 IM_USED"
+        | "300 MULTIPLE_CHOICES"
+        | "301 MOVED_PERMANENTLY"
+        | "302 FOUND"
+        | "302 MOVED_TEMPORARILY"
+        | "303 SEE_OTHER"
+        | "304 NOT_MODIFIED"
+        | "305 USE_PROXY"
+        | "307 TEMPORARY_REDIRECT"
+        | "308 PERMANENT_REDIRECT"
+        | "400 BAD_REQUEST"
+        | "401 UNAUTHORIZED"
+        | "402 PAYMENT_REQUIRED"
+        | "403 FORBIDDEN"
+        | "404 NOT_FOUND"
+        | "405 METHOD_NOT_ALLOWED"
+        | "406 NOT_ACCEPTABLE"
+        | "407 PROXY_AUTHENTICATION_REQUIRED"
+        | "408 REQUEST_TIMEOUT"
+        | "409 CONFLICT"
+        | "410 GONE"
+        | "411 LENGTH_REQUIRED"
+        | "412 PRECONDITION_FAILED"
+        | "413 PAYLOAD_TOO_LARGE"
+        | "413 REQUEST_ENTITY_TOO_LARGE"
+        | "414 URI_TOO_LONG"
+        | "414 REQUEST_URI_TOO_LONG"
+        | "415 UNSUPPORTED_MEDIA_TYPE"
+        | "416 REQUESTED_RANGE_NOT_SATISFIABLE"
+        | "417 EXPECTATION_FAILED"
+        | "418 I_AM_A_TEAPOT"
+        | "419 INSUFFICIENT_SPACE_ON_RESOURCE"
+        | "420 METHOD_FAILURE"
+        | "421 DESTINATION_LOCKED"
+        | "422 UNPROCESSABLE_ENTITY"
+        | "423 LOCKED"
+        | "424 FAILED_DEPENDENCY"
+        | "425 TOO_EARLY"
+        | "426 UPGRADE_REQUIRED"
+        | "428 PRECONDITION_REQUIRED"
+        | "429 TOO_MANY_REQUESTS"
+        | "431 REQUEST_HEADER_FIELDS_TOO_LARGE"
+        | "451 UNAVAILABLE_FOR_LEGAL_REASONS"
+        | "500 INTERNAL_SERVER_ERROR"
+        | "501 NOT_IMPLEMENTED"
+        | "502 BAD_GATEWAY"
+        | "503 SERVICE_UNAVAILABLE"
+        | "504 GATEWAY_TIMEOUT"
+        | "505 HTTP_VERSION_NOT_SUPPORTED"
+        | "506 VARIANT_ALSO_NEGOTIATES"
+        | "507 INSUFFICIENT_STORAGE"
+        | "508 LOOP_DETECTED"
+        | "509 BANDWIDTH_LIMIT_EXCEEDED"
+        | "510 NOT_EXTENDED"
+        | "511 NETWORK_AUTHENTICATION_REQUIRED";
+      /** @description Beskrivelse av hva som har gått galt. */
+      melding?: string;
     };
     /** @description Feil som har oppstått pga optimistisk lås. */
     ConflictResponse: {
@@ -648,84 +772,6 @@ export interface components {
       /** @description Beskrivelse av hva som har gått galt. */
       melding?: string;
     };
-    ApiErrorResponse: {
-      /**
-       * @description HttpStatus for responsen.
-       * @enum {string}
-       */
-      httpStatus:
-        | "100 CONTINUE"
-        | "101 SWITCHING_PROTOCOLS"
-        | "102 PROCESSING"
-        | "103 EARLY_HINTS"
-        | "103 CHECKPOINT"
-        | "200 OK"
-        | "201 CREATED"
-        | "202 ACCEPTED"
-        | "203 NON_AUTHORITATIVE_INFORMATION"
-        | "204 NO_CONTENT"
-        | "205 RESET_CONTENT"
-        | "206 PARTIAL_CONTENT"
-        | "207 MULTI_STATUS"
-        | "208 ALREADY_REPORTED"
-        | "226 IM_USED"
-        | "300 MULTIPLE_CHOICES"
-        | "301 MOVED_PERMANENTLY"
-        | "302 FOUND"
-        | "302 MOVED_TEMPORARILY"
-        | "303 SEE_OTHER"
-        | "304 NOT_MODIFIED"
-        | "305 USE_PROXY"
-        | "307 TEMPORARY_REDIRECT"
-        | "308 PERMANENT_REDIRECT"
-        | "400 BAD_REQUEST"
-        | "401 UNAUTHORIZED"
-        | "402 PAYMENT_REQUIRED"
-        | "403 FORBIDDEN"
-        | "404 NOT_FOUND"
-        | "405 METHOD_NOT_ALLOWED"
-        | "406 NOT_ACCEPTABLE"
-        | "407 PROXY_AUTHENTICATION_REQUIRED"
-        | "408 REQUEST_TIMEOUT"
-        | "409 CONFLICT"
-        | "410 GONE"
-        | "411 LENGTH_REQUIRED"
-        | "412 PRECONDITION_FAILED"
-        | "413 PAYLOAD_TOO_LARGE"
-        | "413 REQUEST_ENTITY_TOO_LARGE"
-        | "414 URI_TOO_LONG"
-        | "414 REQUEST_URI_TOO_LONG"
-        | "415 UNSUPPORTED_MEDIA_TYPE"
-        | "416 REQUESTED_RANGE_NOT_SATISFIABLE"
-        | "417 EXPECTATION_FAILED"
-        | "418 I_AM_A_TEAPOT"
-        | "419 INSUFFICIENT_SPACE_ON_RESOURCE"
-        | "420 METHOD_FAILURE"
-        | "421 DESTINATION_LOCKED"
-        | "422 UNPROCESSABLE_ENTITY"
-        | "423 LOCKED"
-        | "424 FAILED_DEPENDENCY"
-        | "425 TOO_EARLY"
-        | "426 UPGRADE_REQUIRED"
-        | "428 PRECONDITION_REQUIRED"
-        | "429 TOO_MANY_REQUESTS"
-        | "431 REQUEST_HEADER_FIELDS_TOO_LARGE"
-        | "451 UNAVAILABLE_FOR_LEGAL_REASONS"
-        | "500 INTERNAL_SERVER_ERROR"
-        | "501 NOT_IMPLEMENTED"
-        | "502 BAD_GATEWAY"
-        | "503 SERVICE_UNAVAILABLE"
-        | "504 GATEWAY_TIMEOUT"
-        | "505 HTTP_VERSION_NOT_SUPPORTED"
-        | "506 VARIANT_ALSO_NEGOTIATES"
-        | "507 INSUFFICIENT_STORAGE"
-        | "508 LOOP_DETECTED"
-        | "509 BANDWIDTH_LIMIT_EXCEEDED"
-        | "510 NOT_EXTENDED"
-        | "511 NETWORK_AUTHENTICATION_REQUIRED";
-      /** @description Beskrivelse av hva som har gått galt. */
-      melding?: string;
-    };
     /** @description Representasjon av audit info for et objekt. */
     AuditInfoResponse: {
       /**
@@ -806,7 +852,7 @@ export interface components {
        * @description HttpStatus for responsen
        * @enum {string}
        */
-      httpStatus?:
+      httpStatus:
         | "100 CONTINUE"
         | "101 SWITCHING_PROTOCOLS"
         | "102 PROCESSING"
@@ -880,9 +926,9 @@ export interface components {
       geometryType?: string;
       /** @description Valideringsfeil */
       validationError?: string;
-      feilLokasjon?: components["schemas"]["Point"];
+      feilLokasjon: components["schemas"]["Point"];
       /** @description Generell beskrivelse */
-      message?: string;
+      message: string;
     };
     /** @description En referanse til et utkast */
     UtkastRef: {
@@ -892,6 +938,11 @@ export interface components {
       navn: string;
       /** @description URL til full representasjon av utkastet. */
       href: string;
+      /**
+       * Format: date-time
+       * @description Tidspunktet utkastet ble opprettet.
+       */
+      opprettetDato: string;
     };
     /** @description Geometrien til fylket. Ikke grenser */
     FeatureCollection: {
@@ -1871,6 +1922,33 @@ export interface operations {
       };
     };
   };
+  /** Henter grensene til en stemmekrets med gitt id */
+  hentGrenserForStemmekrets_1: {
+    parameters: {
+      path: {
+        /** ID-en til stemmekretsen man vil hente */
+        id: string;
+      };
+      query: {
+        /** Eventuell gyldighetsdato for stemmekrets (default = dagens dato */
+        gyldighetsdato?: string;
+      };
+    };
+    responses: {
+      /** Successful operation */
+      200: {
+        content: {
+          "application/json": components["schemas"]["FeatureCollection"];
+        };
+      };
+      /** Not Found */
+      404: {
+        content: {
+          "application/json": components["schemas"]["FeatureCollection"];
+        };
+      };
+    };
+  };
   /** Henter alle kommuner i Nasjonal inndelingsbase. */
   hentKommuner_1: {
     parameters: {
@@ -1992,6 +2070,32 @@ export interface operations {
       404: {
         content: {
           "application/json": components["schemas"]["KretsResponse"][];
+        };
+      };
+    };
+  };
+  hentGrenserForGrunnkrets_1: {
+    parameters: {
+      path: {
+        /** ID-en til grunnkretsen man vil hente */
+        id: string;
+      };
+      query: {
+        /** Eventuell gyldighetsdato for grunnkrets (default = dagens dato */
+        gyldighetsdato?: string;
+      };
+    };
+    responses: {
+      /** Successful operation */
+      200: {
+        content: {
+          "application/json": components["schemas"]["FeatureCollection"];
+        };
+      };
+      /** Not Found */
+      404: {
+        content: {
+          "application/json": components["schemas"]["FeatureCollection"];
         };
       };
     };
