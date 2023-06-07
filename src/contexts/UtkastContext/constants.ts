f2b3808faa016c59f9978b22cef43495fc8f6be9
Steffen Hageland <steffen.hageland@bekk.no>
2023-06-07 12:24:58 +0200

    TS-1090 Legger til kvalitetsheving som endringstype på utkast

--- src/contexts/UtkastContext/constants.ts
+++ src/contexts/UtkastContext/constants.ts
@@ -3,6 +3,7 @@ export const translateKeysByEndringsType: Record<string, string> = {
   "Vedtatt sammenslåing": "utkast.Vedtatt sammenslåing",
   "Vedtatt deling": "utkast.Vedtatt deling",
   Fastsetting: "utkast.Fastsetting",
+  Kvalitetsheving: "utkast.Kvalitetsheving",
   Navneendring: "utkast.Navneendring",
   Nummerendring: "utkast.Nummerendring",
   Retting: "utkast.Retting",
