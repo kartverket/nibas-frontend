67ba8c8335ba3d445c4daad18b92bcd50a6b7106
Anders Larsen <andelar96@gmail.com>
2023-06-15 15:54:51 +0200

    Oppdaterer rot

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 import "./i18n";
 
 // TODO: kvitte oss med denne i react 18? skjer noe med dependency da?
-ReactDOM.render(
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(document.getElementById("root")!);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
