10fcc6f5e93156d0655664eaea6e721dbe5f7d66
Anders Larsen <andelar96@gmail.com>
2023-06-16 10:18:50 +0200

    Fjerner pakker

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
-import "./i18n";
 
-// TODO: kvitte oss med denne i react 18? skjer noe med dependency da?
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(document.getElementById("root")!);
 root.render(
--- src/test/test-setup.ts
+++ src/test/test-setup.ts
@@ -35,18 +35,3 @@ afterEach(() => {
 afterAll(() => {
   server.close();
 });
-
-vi.mock("react-i18next", () => ({
-  // this mock makes sure any components using the translate hook can use it without a warning being shown
-  useTranslation: () => {
-    return {
-      t: (str: string) => str,
-      i18n: {
-        changeLanguage: () =>
-          new Promise(() => {
-            // do nothing
-          }),
-      },
-    };
-  },
-}));
