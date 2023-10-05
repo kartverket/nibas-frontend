c3316d96b65a6f28c6b739dffc8eaf110c9faa86
Anders Larsen <andelar96@gmail.com>
2023-10-05 17:08:40 +0200

    (fikser git-bug)

--- src/pages/Kart/interactions/useSelect.ts
+++ src/pages/Kart/interactions/useSelect.ts
@@ -80,6 +80,7 @@ const useSelect = () => {
         !borderIsEditable(clickedFeature)
       ) {
         toast({ status: "error", title: "Denne grensen er ikke redigerbar" });
+        event.stopPropagation();
         return;
       }
 
