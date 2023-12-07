8e315718d73f7c59769c316a0d71fbf063bcfbc9
Anders Larsen <andelar96@gmail.com>
2023-12-07 19:21:44 +0100

    Bytt ut react-dnd med pilknapper

--- src/pages/Kart/OverlayPanels/Kartlag/ActiveKartlag/ActiveKartlag.tsx
+++ src/pages/Kart/OverlayPanels/Kartlag/ActiveKartlag/ActiveKartlag.tsx
@@ -1,59 +1,88 @@
 import React from "react";
 import { styled } from "styled-components";
-import { useDragAndDrop } from "../utils";
-import { Heading, IconButton, Divider, ButtonGroup, Icon } from "@kvib/react";
+import {
+  Heading,
+  IconButton,
+  Divider,
+  ButtonGroup,
+  Spinner,
+} from "@kvib/react";
 import ActiveKartlagOpacity from "./ActiveKartlagOpacity";
 import { VisibleLayer } from "contexts/KartlagContext/useVisibleLayers";
 import { useKartlag } from "contexts/KartlagContext/KartlagContext";
 
 type Props = {
   index: number;
+  maxIndex: number;
   layer: VisibleLayer;
 };
 
-const ActiveKartlag = ({ layer, index }: Props) => {
+const ActiveKartlag = ({ layer, index, maxIndex }: Props) => {
   const { mappedLayers, moveLayer, toggleLayerVisibility } = useKartlag();
 
   const mappedLayer = mappedLayers.find(
     (ml) => ml.sourceId === layer.mainLayer,
   );
 
-  const ref = useDragAndDrop(index, mappedLayer, moveLayer);
-
   return (
-    <Container ref={ref}>
-      <DragIcon icon="format_line_spacing" />
-      <Card>
-        <TitleContainer>
-          <Heading as="h4" size="sm">
-            {mappedLayer?.title}
-          </Heading>
-          <Buttons>
-            <ActiveKartlagOpacity layerId={layer.mainLayer} />
+    <>
+      {mappedLayer ? (
+        <Container>
+          <ArrowButtons>
             <IconButton
-              aria-label={`Fjern ${layer.mainLayer}`}
-              variant="ghost"
-              icon="close"
-              onClick={() => toggleLayerVisibility(layer.mainLayer)}
+              variant="secondary"
+              colorScheme="gray"
+              icon="arrow_upward"
+              aria-label="Flytt kartlag opp"
+              onClick={() => moveLayer("up", mappedLayer.sourceId)}
+              isDisabled={index === 0}
             />
-          </Buttons>
-        </TitleContainer>
-        {layer.subLayers.map((subLayer) => (
-          <React.Fragment key={subLayer}>
-            <Divider />
-            <ActiveSublag>
-              <span>{subLayer}</span>
-              <IconButton
-                aria-label={`Fjern ${subLayer}`}
-                variant="ghost"
-                icon="close"
-                onClick={() => toggleLayerVisibility(layer.mainLayer, subLayer)}
-              />
-            </ActiveSublag>
-          </React.Fragment>
-        ))}
-      </Card>
-    </Container>
+            <IconButton
+              variant="secondary"
+              colorScheme="gray"
+              icon="arrow_downward"
+              aria-label="Flytt kartlag ned"
+              onClick={() => moveLayer("down", mappedLayer.sourceId)}
+              isDisabled={index === maxIndex}
+            />
+          </ArrowButtons>
+          <Card>
+            <TitleContainer>
+              <Heading as="h4" size="sm">
+                {mappedLayer.title}
+              </Heading>
+              <Buttons>
+                <ActiveKartlagOpacity layerId={layer.mainLayer} />
+                <IconButton
+                  aria-label={`Fjern ${layer.mainLayer}`}
+                  variant="ghost"
+                  icon="close"
+                  onClick={() => toggleLayerVisibility(layer.mainLayer)}
+                />
+              </Buttons>
+            </TitleContainer>
+            {layer.subLayers.map((subLayer) => (
+              <React.Fragment key={subLayer}>
+                <Divider />
+                <ActiveSublag>
+                  <span>{subLayer}</span>
+                  <IconButton
+                    aria-label={`Fjern ${subLayer}`}
+                    variant="ghost"
+                    icon="close"
+                    onClick={() =>
+                      toggleLayerVisibility(layer.mainLayer, subLayer)
+                    }
+                  />
+                </ActiveSublag>
+              </React.Fragment>
+            ))}
+          </Card>
+        </Container>
+      ) : (
+        <Spinner />
+      )}
+    </>
   );
 };
 
@@ -63,8 +92,10 @@ const Container = styled.div`
   gap: 16px;
 `;
 
-const DragIcon = styled(Icon)`
-  cursor: move;
+const ArrowButtons = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
 `;
 
 const Card = styled.div`
