1cb0700849411051b38247280965c7dee175e733
Steffen Hageland <etse88@gmail.com>
2024-01-02 13:34:59 +0100

    Styling på feilmeldingen

--- src/components/FrontendLogger/FrontendLoggerErrorBoundry.tsx
+++ src/components/FrontendLogger/FrontendLoggerErrorBoundry.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Outlet } from "react-router";
 import frontendLogger from "./FrontendLogger";
+import { Card, Heading, Logo, Text } from "@kvib/react";
+import { styled } from "styled-components";
+import ThirdPartyProviders from "pages/App/ThirdPartyProviders";
 
 type ErrorState = {
   hasError: boolean;
@@ -12,7 +15,7 @@ export class ErrorBoundaryWithFrontendLogger extends React.Component<
 > {
   constructor(props: unknown) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { hasError: true };
   }
 
   static getDerivedStateFromError() {
@@ -25,15 +28,47 @@ export class ErrorBoundaryWithFrontendLogger extends React.Component<
   render() {
     if (this.state.hasError) {
       return (
-        <section>
-          <h1>En ukjent feil har oppstått</h1>
-          <p>
-            Noe gikk galt med siden, men vi vet ikke helt hvorfor. Vennligst
-            oppdater siden eller send oss en melding om feilen vedvarer.
-          </p>
-        </section>
+        <ThirdPartyProviders>
+          <ErrorContainer>
+            <LogoContainer>
+              <Logo variant="vertical" size={128} />
+            </LogoContainer>
+            <FeilCard>
+              <Heading as="h2" size="lg" mb="32px">
+                En ukjent feil har oppstått
+              </Heading>
+              <Text fontSize="lg">
+                Noe gikk galt med siden, men vi vet ikke helt hvorfor.
+              </Text>
+              <Text fontSize="lg">
+                Vennligst oppdater siden eller send oss en melding om feilen
+                vedvarer.
+              </Text>
+            </FeilCard>
+          </ErrorContainer>
+        </ThirdPartyProviders>
       );
     }
     return <Outlet />;
   }
 }
+
+const ErrorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  min-height: 100vh;
+  background: var(--kvib-colors-gray-50);
+`;
+
+const LogoContainer = styled.div`
+  margin-top: 180px;
+  margin-bottom: 80px;
+`;
+
+const FeilCard = styled(Card)`
+  text-align: center;
+  padding: 50px;
+  max-width: 800px;
+`;
