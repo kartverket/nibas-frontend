1c9a61c5e685c26276d2ae175f640e67eec44da5
chgr007 <71885781+chgr007@users.noreply.github.com>
2024-01-26 12:39:59 +0100

    Beveger meg bort fra gammel kode

---
+++ src/pages/Kart/OverlayPanels/MetadataPanel/dokref_old/DokrefField.tsx
@@ -0,0 +1,29 @@
+import { DokrefRow } from "../DokrefRow";
+import { Feature } from "ol";
+
+type Props = {
+  children: React.ReactNode;
+  name: string;
+  tooltipLabel: string;
+  feature: Feature;
+  fieldKey: keyof DokrefForm;
+};
+
+export type DokrefForm = {
+  apiId?: string;
+  dokumentlenker: string[];
+  fastsettingsdato: string;
+  fastsettingsmyndighet?: string;
+  hjemmel?: string;
+  internReferanserKartverket: string[];
+  rettskildeId?: string;
+  rettskildeTittel: string;
+};
+
+export const DokrefField = ({ children, name, tooltipLabel }: Props) => {
+  return (
+    <DokrefRow tooltipLabel={tooltipLabel} name={name}>
+      {children}
+    </DokrefRow>
+  );
+};
