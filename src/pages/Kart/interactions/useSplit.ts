26e04f95e1d15f574b88ec47fa5506a166876576
Steffen Hageland <steffen.hageland@kartverket.no>
2024-02-02 10:35:15 +0100

    Mulighet for å splitte grenser

--- src/pages/Kart/interactions/useSplit.ts
+++ src/pages/Kart/interactions/useSplit.ts
@@ -1,9 +1,13 @@
 import LineString from "ol/geom/LineString";
 import { useHistory } from "contexts/HistoryContext";
-import { addFeaturesToSource } from "utils/map/source";
+import { addFeaturesToSource, removeFeaturesFromSourceByIds } from "utils/map/source";
 import { useToolbar } from "contexts/ToolbarContext";
 import { Point } from "ol/geom";
 import { useFeatureStyle } from "contexts/FeatureStyleContext";
+import { getTempFeatureId, isTempFeatureId } from "pages/Kart/interactions/tempFeatureIdUtil";
+import { Feature } from "ol";
+import Geometry from "ol/geom/Geometry";
+import { FeatureProperties } from "../../../types/api";
 
 // OBS! Per nå skiller denne seg fra de andre interaksjonene ved at den ikke legges til som et event i kartet
 // I stedet bruker man select og selectPoint etter hverandre, og utløser handlingen ved en knapp i React
@@ -12,6 +16,23 @@ const useSplit = () => {
   const { activeTool } = useToolbar();
   const { selectedFeatures, selectedPoint } = useFeatureStyle();
 
+  const createCloneOfFeatureWithPartsOfCoordinates = (
+    feature: Feature,
+    startIndex: number,
+    endIndex: number,
+  ): Feature<Geometry> => {
+    const newFeature = feature.clone();
+    const newFeatureId = getTempFeatureId();
+    const newGeometry = newFeature.getGeometry() as LineString;
+
+    // Siden OL er mutable og vi ikke ønsker å mutere den eksisterende geometrien
+    const coordinates = [...newGeometry.getCoordinates()];
+    const newCoordinates = coordinates.splice(startIndex, endIndex);
+    newFeature.setId(newFeatureId);
+    newGeometry.setCoordinates(newCoordinates);
+    return newFeature;
+  };
+
   const split = () => {
     if (activeTool === "split" && selectedFeatures.length === 1 && selectedPoint) {
       const feature = selectedFeatures[0];
@@ -19,9 +40,6 @@ const useSplit = () => {
       if (geometry instanceof LineString) {
         const coordinates = geometry.getCoordinates();
         const featureId = feature.getId() as string;
-
-        // Siden OL-objekter er mutable og vi trenger dette til senere:
-        const originalCoordinates = [...coordinates];
 
         // Ikke vits å gjøre splitting med mindre du har en linje med minst tre punkter
         if (coordinates.length > 2) {
@@ -35,38 +53,20 @@ const useSplit = () => {
 
             // Dette verifiserer at det valgte punktet er et gyldig punkt å splitte på grensen
             if (splitIndex > 0 && splitIndex < coordinates.length - 1) {
-              // TODO: alt under denne kommentaren er spekulasjon på hvordan vi kanskje ønsker å gjøre det på sikt
-              // per nå er ikke split faktisk brukbart uten backend uansett, så det er bare et forslag
-              const clonedFeature = feature.clone();
-              const clonedFeatureId = `${featureId}-clone`;
-              const clonedGeometry = clonedFeature.getGeometry() as LineString;
-              const clonedCoordinates = clonedGeometry.getCoordinates();
-              clonedFeature.setId(clonedFeatureId);
+              const newFeature1 = createCloneOfFeatureWithPartsOfCoordinates(feature, 0, splitIndex + 1);
+              const newFeature2 = createCloneOfFeatureWithPartsOfCoordinates(feature, splitIndex, coordinates.length);
 
-              const headCoordinates = coordinates.splice(0, splitIndex + 1);
-              const tailCoordinates = clonedCoordinates.splice(splitIndex);
+              const properties = feature.getProperties() as FeatureProperties;
+              feature.setProperties({ ...properties, shouldArchive: true });
+              addFeaturesToSource("edit", [newFeature1, newFeature2]);
+              addHistoryEntry({
+                type: "grensesplitting",
+                changes: [{ id: featureId, from: [feature], to: [newFeature1, newFeature2] }],
+              });
 
-              geometry.setCoordinates(headCoordinates);
-              clonedGeometry.setCoordinates(tailCoordinates);
-
-              addFeaturesToSource("edit", [clonedFeature]);
-
-              if (featureId && clonedFeatureId) {
-                addHistoryEntry({
-                  type: "grense",
-                  changes: [
-                    {
-                      id: featureId,
-                      from: { coordinates: originalCoordinates },
-                      to: { coordinates },
-                    },
-                    {
-                      id: clonedFeatureId,
-                      from: { coordinates: [] },
-                      to: { coordinates: clonedCoordinates },
-                    },
-                  ],
-                });
+              // Hvis featuren som ble splittet er en ny feature så ønsker vi ikke vise den som arkivert
+              if (isTempFeatureId(featureId)) {
+                removeFeaturesFromSourceByIds("edit", [featureId]);
               }
             }
           }
