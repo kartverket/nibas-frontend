d71d779c2f1c61cae987b4f2f529bea97f1a3936
anderssonw <wandersson.98@gmail.com>
2024-02-05 14:50:16 +0100

    administrative grenser kan ikke endres om man ikke viser relaterte kretser
    Signed-off-by: anderssonw <wandersson.98@gmail.com>

--- src/utils/features.ts
+++ src/utils/features.ts
@@ -1,8 +1,12 @@
 import { GrenseType, getEditingTypeFromGrenseType } from "hooks/layers/types";
+import { MetadataDiscriminator, getGrenseDiscriminatorFromType, isAdministrativGrense } from "./grenser";
 import { Feature } from "ol";
 import { Geometry } from "ol/geom";
-import { FeatureProperties, Metadata } from "types/api";
-import { MetadataDiscriminator, getGrenseDiscriminatorFromType } from "./grenser";
+import { FeatureProperties, KontekstEgenskaper, Metadata } from "types/api";
+import { FeatureLike } from "ol/Feature";
+import { grenserLayers, editableBorderTypes } from "hooks/layers/constants";
+import { isNotNullOrUndefined } from "types/common";
+import { getRepresentasjonspunktId } from "./map/source";
 
 export const setDefaultFeatureProperties = (feature: Feature<Geometry>, grenseType: GrenseType | undefined) => {
   if (!grenseType) return;
@@ -56,4 +60,39 @@ export const getDefaultFeatureProperties = (grenseType: GrenseType): FeatureProp
   };
 
   return properties;
+};
+
+export const isFeatureEditable = (feature: FeatureLike, isArchived: boolean) => {
+  const featureType = feature.get("type") as GrenseType;
+
+  if (isAdministrativGrense(featureType)) {
+    const properties = feature.getProperties() as FeatureProperties;
+    const kontekstEgenskaper = properties.kontekstEgenskaper as KontekstEgenskaper[];
+
+    if (!kontekstEgenskaper || kontekstEgenskaper.length == 0) return false;
+
+    const layerSources = Object.values(grenserLayers)
+      .map((layer) => layer.getSource())
+      .filter(isNotNullOrUndefined);
+
+    // Kontekstegenskaper inneholder hvilke kretser som grensen tilhører (f. eks stemme/grunnkrets)
+    // Alle disse kretsene må være synlige for at en administrativ grense skal være synlig
+    // Vi kan sjekke gjennom alle layer sources for å se om lokalIDen til representasjonspunktet er tilgjengelig som en feature.
+    // Dersom den er tilgjengelig, kan vi anta at kretsen er synlig
+    const alleKretserIKontekstEgenskaperErSynlig = kontekstEgenskaper.every((egenskap) => {
+      const lokalId = egenskap.id?.lokalid.value;
+      if (!lokalId) return false;
+
+      // TODO Velge riktig layerSource basert på kontekstegenskaptype?
+      return layerSources.some((source) => {
+        return source.getFeatureById(getRepresentasjonspunktId(lokalId)) != null;
+      });
+    });
+
+    if (!alleKretserIKontekstEgenskaperErSynlig) return false;
+  }
+
+  const isEditableFeatureType = editableBorderTypes.includes(featureType);
+
+  return isEditableFeatureType && !isArchived;
 };
