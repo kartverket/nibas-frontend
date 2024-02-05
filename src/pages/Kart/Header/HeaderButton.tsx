eae7bbe6cb9a329f9d60c3c864518c0ef83e7c82
plusk <andelar96@gmail.com>
2024-02-05 13:15:15 +0100

    Mindre custom header button

--- src/pages/Kart/Header/HeaderButton.tsx
+++ src/pages/Kart/Header/HeaderButton.tsx
@@ -1,5 +1,4 @@
-import { IconButton, MaterialSymbol, Tooltip } from "@kvib/react";
-import { styled } from "styled-components";
+import { Button, MaterialSymbol, Tooltip } from "@kvib/react";
 import { TooltipBody } from "../Toolbar/CustomTooltip";
 import { Shortcut } from "hooks/keyboard-shortcuts/keyboard-shortcuts";
 
@@ -14,36 +13,10 @@ type HeaderButtonProps = {
 
 const HeaderButton = ({ icon, label, labelIsHidden, onClick, isDisabled, tooltip }: HeaderButtonProps) => (
   <Tooltip hasArrow label={<TooltipBody text={tooltip.text} shortcut={tooltip.shortcut} />} isDisabled={!tooltip}>
-    <Label $isDisabled={isDisabled}>
-      <HeaderIconButton
-        variant="secondary"
-        colorScheme="gray"
-        icon={icon}
-        aria-label={label}
-        onClick={onClick}
-        isDisabled={isDisabled}
-      />
+    <Button size="xs" variant="ghost" leftIcon={icon} aria-label={label} onClick={onClick} isDisabled={isDisabled}>
       {!labelIsHidden && label}
-    </Label>
+    </Button>
   </Tooltip>
 );
-
-const HeaderIconButton = styled(IconButton)`
-  min-width: unset;
-  height: unset;
-  padding: 5px;
-
-  & > .material-symbols-rounded {
-    font-size: var(--kvib-fontSizes-lg) !important;
-  }
-`;
-
-const Label = styled.label<{ $isDisabled?: boolean }>`
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  white-space: nowrap;
-  cursor: ${(props) => (props.$isDisabled ? "not-allowed" : "pointer")};
-`;
 
 export default HeaderButton;
