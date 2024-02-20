cd1f31f0d48d198c9e834e68197a2285bc4ec7d3
Christian Gregersen <71885781+chgr007@users.noreply.github.com>
2024-02-20 12:28:01 +0100

    Update src/pages/Kart/OverlayPanels/GrenseinformasjonPanel/Vedtaksinformasjon/VedtaksinfoDetaljer.tsx
    Co-authored-by: Anders Larsen <andelar96@gmail.com>

--- src/pages/Kart/OverlayPanels/GrenseinformasjonPanel/Vedtaksinformasjon/VedtaksinfoDetaljer.tsx
+++ src/pages/Kart/OverlayPanels/GrenseinformasjonPanel/Vedtaksinformasjon/VedtaksinfoDetaljer.tsx
@@ -120,6 +120,7 @@ export const VedtaksinfoDetaljer = ({
 
   return (
     <Modal isOpen={isOpen} onClose={closeModal} isCentered size={"6xl"}>
+      <ModalOverlay />
       <ModalContent>
         <form onSubmit={handleSubmit(onSubmit)}>
           <BorderBottom>
