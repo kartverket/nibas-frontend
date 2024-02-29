229951e538e1a381d754c0ac83b4af23d23ff345
Aleksander Karlsen <aleweskar@gmail.com>
2024-02-29 11:43:02 +0100

    Add dragZoom interaksjon

--- src/pages/Kart/interactions/useDragPan.ts
+++ src/pages/Kart/interactions/useDragPan.ts
@@ -1,19 +1,34 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useToolbar } from "contexts/ToolbarContext";
-import { DragPan } from "ol/interaction";
+import { DragPan, DragZoom } from "ol/interaction";
+import { useHoldButtonToggle } from "hooks/keyboard-shortcuts/keyboard-shortcuts-hook";
 
 const useDragPan = () => {
   const { activeModeTools } = useToolbar();
 
+  const [isShiftPressed, setIsShiftPressed] = useState(false);
+
+  // må disable dargPan iteraksjonen ved keypress av shift slik at brukeren ikke kan panorere når man prøver å markere et område
+  useHoldButtonToggle(
+    "shift",
+    isShiftPressed,
+    () => setIsShiftPressed(true),
+    () => setIsShiftPressed(false),
+  );
+
   const dragPan = useMemo(
     () =>
       new DragPan({
-        condition: () => activeModeTools.includes("move"),
+        condition: () => activeModeTools.includes("move") && !isShiftPressed,
       }),
-    [activeModeTools],
+    [activeModeTools, isShiftPressed],
   );
 
-  return { dragPan };
+  const dragZoom = new DragZoom({
+    condition: () => activeModeTools.includes("move") && isShiftPressed,
+  });
+
+  return { dragPan, dragZoom };
 };
 
 export default useDragPan;
