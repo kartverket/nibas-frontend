9e4f52a5cc3a4368036c4e85256106c7a29da6d6
Christian Gregersen <71885781+chgr007@users.noreply.github.com>
2024-03-05 13:13:16 +0100

    Update src/pages/Kart/interactions/useDraw.ts

--- src/pages/Kart/interactions/useDraw.ts
+++ src/pages/Kart/interactions/useDraw.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo } from "react";
 import Draw, { DrawEvent } from "ol/interaction/Draw";
 import { pixelTolerance } from "./constants";
 import { useToolbar } from "contexts/ToolbarContext";
