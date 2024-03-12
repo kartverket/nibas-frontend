02698df7f9a763f7f92d695be6690cdf13931cb5
chgr007 <71885781+chgr007@users.noreply.github.com>
2024-03-12 08:36:13 +0100

    TS-1527 valgfri snapping. Lagt til hover når rediger er valgt uten verktøy & lagt til escape for menyen

--- src/pages/Kart/Toolbar/Toolbar.tsx
+++ src/pages/Kart/Toolbar/Toolbar.tsx
@@ -1,4 +1,15 @@
-import { Checkbox, CloseButton, Divider, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Switch } from "@kvib/react";
+import {
+  Button,
+  Checkbox,
+  CloseButton,
+  Divider,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Switch,
+} from "@kvib/react";
 import { useEditAllGrenser } from "contexts/EditGrenserContext";
 import { useOverlayPanel } from "contexts/OverlayPanelContext";
 import { useToolbar } from "contexts/ToolbarContext";
@@ -113,6 +124,7 @@ const Toolbar = () => {
     isPanningAllowed,
   );
 
+  const [isSnappingMenuOpen, setIsSnappingMenuOpen] = useState(false);
   // Alt her er en prioritert rekkefølge på hva som bør "exites" først. Det kan sikkert itereres litt på, men dette er et foreløpig forslag
   useKeyboardShortcut("escape", () => {
     if (activeTool === "draw") {
@@ -129,6 +141,11 @@ const Toolbar = () => {
           return;
         }
       }
+    }
+
+    if (isSnappingMenuOpen) {
+      setIsSnappingMenuOpen(false);
+      return;
     }
 
     if (activeOverlayPanel) {
@@ -220,13 +237,19 @@ const Toolbar = () => {
             Matrikkel
           </ToolbarButton>
           <ConditionalHide below="xl" condition={!!activeOverlayPanel}>
-            <Menu closeOnSelect={false} closeOnBlur={false}>
+            <Menu
+              closeOnSelect={false}
+              closeOnBlur={false}
+              onClose={() => setIsSnappingMenuOpen(false)}
+              isOpen={isSnappingMenuOpen}
+            >
               {({ onClose }) => {
                 return (
                   <>
                     <MenuButton
+                      onClick={() => setIsSnappingMenuOpen(!isSnappingMenuOpen)}
+                      isActive={activeModeTools.includes("snap_nibas") || activeModeTools.includes("snap_matrikkel")}
                       as={ToolbarButton}
-                      isActive={false}
                       aria-label="Snap til kartlag"
                       icon="layers"
                       tooltip={{ text: "Skru av/på snapping mot kartlag." }}
