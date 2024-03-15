de1d785fa2e5d79e72c24e0e495da43595958c30
chgr007 <71885781+chgr007@users.noreply.github.com>
2024-03-15 13:15:03 +0100

    TS-1590 Fikset bug for dead ends og refactoret

--- src/contexts/HistoryContext/useHistoryState.ts
+++ src/contexts/HistoryContext/useHistoryState.ts
@@ -1,5 +1,8 @@
 import { useCallback, useState } from "react";
 import { HistoryState, HistoryEntry } from "./types";
+import { removeFeatureFromAllLayers } from "utils/features";
+import { getChangeIds } from "./history-utils";
+import { isTempFeatureId } from "pages/Kart/interactions/temp-feature-id-utils";
 
 type Options = {
   onUndo: (entry: HistoryEntry) => void;
@@ -9,14 +12,34 @@ type Options = {
 const useHistoryState = ({ onUndo, onRedo }: Options) => {
   const [history, setHistory] = useState<HistoryState>({ index: 0, entries: [] });
 
+  // Dersom applikasjonen er i tilstanden endring -> angre -> endring, kan man ende opp med features i en source
+  // som ikke er representert i hverken history eller database. Dette vil lage "usynlig" geometri som skaper bugs i beregninger.
+  const clearFeaturesAfterIndex = useCallback(() => {
+    if (history.entries.length === 0) return;
+
+    const entriesBeforeIndex = history.entries.slice(0, history.index);
+    const entriesAfterIndex = history.entries.slice(history.index);
+    const changedFeatureIdsAfterIndex: string[] = entriesAfterIndex.flatMap(getChangeIds);
+    const changedFeatureIdsBeforeIndex: string[] = entriesBeforeIndex.flatMap(getChangeIds);
+
+    const featureIdsToRemove = changedFeatureIdsAfterIndex?.filter(
+      (futuresHistoryId) =>
+        isTempFeatureId(futuresHistoryId) &&
+        !changedFeatureIdsBeforeIndex.some((pastHistoryId) => pastHistoryId === futuresHistoryId),
+    );
+
+    featureIdsToRemove.forEach(removeFeatureFromAllLayers);
+  }, [history]);
+
   const addHistoryEntry = useCallback(
     (entry: HistoryEntry) => {
+      clearFeaturesAfterIndex();
       setHistory((prevHistory) => ({
         index: prevHistory.index + 1,
         entries: [...prevHistory.entries.slice(0, prevHistory.index), entry],
       }));
     },
-    [setHistory],
+    [clearFeaturesAfterIndex],
   );
 
   const clearHistory = () => {
