37a864abadf8dc406623c424f9e6d8c688ca4774
William Andersson <wandersson.98@gmail.com>
2024-03-19 12:28:11 +0100

    TS-1610 Tillat minus i koordinat ved flytting (#516)
    Signed-off-by: anderssonw <wandersson.98@gmail.com>

--- src/pages/Kart/OverlayPanels/KoordinaterPanel.test.tsx
+++ src/pages/Kart/OverlayPanels/KoordinaterPanel.test.tsx
@@ -5,34 +5,69 @@ const defaultProps: React.ComponentProps<typeof KoordinaterPanel> = {
   isOpen: true,
 };
 
-describe("KoordinaterPanel", () => {
-  it("it should allow only decimal or integer inputs", async () => {
+describe("KoordinaterPanel", async () => {
+  const user = userEvent.setup();
+
+  it("should allow positive and negative integers", async () => {
     render(<KoordinaterPanel {...defaultProps} />);
 
-    const user = userEvent.setup();
+    const nord = await screen.findByLabelText("Nord");
+    const ost = await screen.findByLabelText("Øst");
+
+    await user.type(nord, "1");
+    await user.type(ost, "-1");
+
+    expect(nord).toBeValid();
+    expect(ost).toBeValid();
+  });
+
+  it("should allow positive and negative decimals", async () => {
+    render(<KoordinaterPanel {...defaultProps} />);
+
     const nord = await screen.findByLabelText("Nord");
     const ost = await screen.findByLabelText("Øst");
 
     await user.type(nord, "1.0");
-    await user.type(ost, ".0");
+    await user.type(ost, "-1.0");
 
     expect(nord).toBeValid();
+    expect(ost).toBeValid();
+  });
+
+  it("should not allow text", async () => {
+    render(<KoordinaterPanel {...defaultProps} />);
+
+    const nord = await screen.findByLabelText("Nord");
+    const ost = await screen.findByLabelText("Øst");
+
+    await user.type(nord, "e");
+    await user.type(ost, "f");
+
+    expect(nord).toBeInvalid();
     expect(ost).toBeInvalid();
+  });
 
-    await user.type(nord, "1");
-    await user.type(ost, "0.");
+  it("should not allow leading or trailing period", async () => {
+    render(<KoordinaterPanel {...defaultProps} />);
 
-    expect(nord).toBeValid();
+    const nord = await screen.findByLabelText("Nord");
+    const ost = await screen.findByLabelText("Øst");
+
+    await user.type(nord, ".1");
+    await user.type(ost, "1.");
+
+    expect(nord).toBeInvalid();
     expect(ost).toBeInvalid();
+  });
 
-    await user.type(nord, "0");
-    await user.type(ost, "e");
+  it("should not allow whitespace", async () => {
+    render(<KoordinaterPanel {...defaultProps} />);
 
-    expect(nord).toBeValid();
-    expect(ost).toBeInvalid();
+    const nord = await screen.findByLabelText("Nord");
+    const ost = await screen.findByLabelText("Øst");
 
-    await user.type(nord, "0..1");
-    await user.type(ost, "0f1");
+    await user.type(nord, " ");
+    await user.type(ost, "  ");
 
     expect(nord).toBeInvalid();
     expect(ost).toBeInvalid();
