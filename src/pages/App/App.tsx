446b1ba32265d0aa90726c1ddf46c65203c8869d
plusk <andelar96@gmail.com>
2024-03-20 13:01:49 +0100

    Fiks eslint-feil etter test

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -44,8 +44,6 @@ const App = () => {
         {redirectAfterLogout}
         <Route path={routes.authentication} element={<ExternalPage />} />
         <Route element={<ProtectedPage />}>
-
-        
           <Route index element={<Landing />} />
           <Route path={routes.utkast}>
             <Route index element={<Utkast />} />
