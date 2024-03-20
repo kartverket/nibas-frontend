4bccf1caab7f74f485b0374e4a767411eb6a14ef
Aleksander Karlsen <aleweskar@gmail.com>
2024-03-20 14:57:57 +0100

    Fjern logging

--- src/hooks/inndelinger/useStemmekretser.ts
+++ src/hooks/inndelinger/useStemmekretser.ts
@@ -22,11 +22,9 @@ export const useStemmekretser = (stemmekretsIds: string[]) => {
 };
 
 export const useKommuneStemmekretser = (kommuneId: string | null) => {
-  const t = useNibasApi(kommuneId != null ? "/v1/kommuner/{id}/stemmekretser" : null, {
+  return useNibasApi(kommuneId != null ? "/v1/kommuner/{id}/stemmekretser" : null, {
     id: kommuneId!,
   });
-  console.log(t);
-  return t;
 };
 
 export const useToKommunerStemmekretser = (kommuneAId: string | null, kommuneBId: string | null) => {
