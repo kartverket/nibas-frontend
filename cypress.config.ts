060ac256a8b157498deea9bc3057a5d308b7de01
dhlon <daniel.lonar@bekk.no>
2024-03-25 10:09:27 +0100

    fjern env

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,18 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      const environment = process.env.NODE_ENV || 'local';
-      switch (environment) {
-        case 'local':
-          config.baseUrl = 'http://localhost:3000';
-          break;
-        case 'dev':
-          config.baseUrl = 'https://nibas.dev.skip.statkart.no';
-          break;
-        default:
-          throw new Error(`Unknown environment: ${environment}`);
-      }
-      return config;
     },
   },
 });
