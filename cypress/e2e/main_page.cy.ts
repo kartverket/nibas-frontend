3e313e6de620c8cc9de81230d27cda1f75f72f52
dhlon <daniel.lonar@bekk.no>
2024-03-25 16:43:13 +0100

    test

--- cypress/e2e/main_page.cy.ts
+++ cypress/e2e/main_page.cy.ts
@@ -3,9 +3,9 @@ describe("main page", () => {
     cy.visit("");
   });
 
-  it("should load the front page with a greeting header", () => {
-    cy.get("h2").should("contain", "Hva har du lyst til å gjøre nå?");
-  });
+//   it("should load the front page with a greeting header", () => {
+//     cy.get("h2").should("contain", "Hva har du lyst til å gjøre nå?");
+//   });
 
   it("should contain at least one button", () => {
     cy.get("button");
