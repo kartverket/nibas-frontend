eb44f4370dc56cde2ec5c02aad30eb6e9b17af28
plusk <andelar96@gmail.com>
2024-03-26 10:45:01 +0100

    Legg til stylint i linting med en feil

--- src/components/ActionCard.tsx
+++ src/components/ActionCard.tsx
@@ -51,7 +51,7 @@ const Container = styled(Button).attrs({ variant: "ghost" })`
 `;
 
 const Title = styled(Heading)`
-  margin: 0 0 0.5rem;
+  margin: 0 0 0.5rem 0;
 `;
 
 const Description = styled(Text)`
