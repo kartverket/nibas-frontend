df78ee46069f2438d37c569ee08c206acd5c6d81
Aleksander Karlsen <aleweskar@gmail.com>
2024-03-27 16:02:29 +0100

    Add mvp søk

---
+++ src/pages/Kart/OverlayPanels/NavigasjonPanel/InndelingSearch.tsx
@@ -0,0 +1,48 @@
+import { SearchAsync } from "@kvib/react";
+import { NavigasjonProps } from "./NavigasjonPanel";
+import { InndelingResponse } from "types/api";
+import { useInndelingerSearch } from "./useInndelingerSearch";
+
+type InndelingOption = InndelingResponse & {
+  label: string;
+};
+
+export const InndelingSearch = ({ centerOnCoordinate }: NavigasjonProps) => {
+  const searchInndelinger = useInndelingerSearch();
+
+  const mapInndelingResponseToOption = (inndelingResponse: InndelingResponse): InndelingOption => {
+    return {
+      id: inndelingResponse.id,
+      navn: inndelingResponse.navn,
+      nummer: inndelingResponse.nummer,
+      type: inndelingResponse.type,
+      representasjonspunkt: inndelingResponse.representasjonspunkt,
+      label: `${inndelingResponse.nummer} ${inndelingResponse.navn}`,
+    };
+  };
+
+  const handleOnChange = (inndeling: InndelingOption | null) => {
+    const north = inndeling?.representasjonspunkt.y;
+    const east = inndeling?.representasjonspunkt.x;
+    if (north != null && east != null) {
+      centerOnCoordinate(north, east);
+    }
+  };
+
+  const loadResults = async (term: string, resultsCallback: (options: InndelingOption[]) => void) => {
+    if (term.length > 0) {
+      const inndelinger = await searchInndelinger(term);
+      resultsCallback(inndelinger.map(mapInndelingResponseToOption));
+    }
+  };
+
+  return (
+    <SearchAsync
+      size={"md"}
+      placeholder="F.eks. «0301» eller «Eigersund»"
+      onChange={handleOnChange}
+      loadOptions={loadResults}
+      debounceTime={500}
+    />
+  );
+};
