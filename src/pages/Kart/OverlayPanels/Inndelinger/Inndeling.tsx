dc4c0244d092332d48d5f9c4173325f3a9015b38
anderssonw <wandersson.98@gmail.com>
2024-04-08 15:29:59 +0200

    stylelint
    Signed-off-by: anderssonw <wandersson.98@gmail.com>

--- src/pages/Kart/OverlayPanels/Inndelinger/Inndeling.tsx
+++ src/pages/Kart/OverlayPanels/Inndelinger/Inndeling.tsx
@@ -22,10 +22,12 @@ const InndelingButton = styled(Button)`
   padding: 32px 16px;
   color: var(--kvib-colors-black);
   font-weight: var(--kvib-fontWeights-normal);
+
   & > div {
     width: 100%;
     justify-content: space-between;
   }
+
   &[data-active] {
     background: var(--kvib-colors-blue-50);
   }
