37514253aa9ff6931834b16b87da593595a9205a
plusk <andelar96@gmail.com>
2024-04-08 15:18:37 +0200

    Sett navigasjonszoom tilbake til tidligere verdi

--- src/pages/Kart/OverlayPanels/NavigasjonPanel/NavigasjonPanel.tsx
+++ src/pages/Kart/OverlayPanels/NavigasjonPanel/NavigasjonPanel.tsx
@@ -16,7 +16,7 @@ const NavigasjonPanel = ({ isOpen, className }: PanelProps) => {
   const centerOnCoordinate = (north: number | null, east: number | null) => {
     if (north !== null && east !== null) {
       const view = map.getView();
-      view.animate({ duration: 0, center: [east, north], zoom: 12 });
+      view.animate({ duration: 0, center: [east, north], zoom: 18 });
       closeOverlayModal();
     }
   };
