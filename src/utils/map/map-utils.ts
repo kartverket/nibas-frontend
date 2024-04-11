0d3aaf3e34edf3000b10b6003f4215e815994adc
anderssonw <wandersson.98@gmail.com>
2024-04-11 12:24:03 +0200

    export pixeldistance
    Signed-off-by: anderssonw <wandersson.98@gmail.com>

--- src/utils/map/map-utils.ts
+++ src/utils/map/map-utils.ts
@@ -77,7 +77,7 @@ export const getZoomMode = (isEditing: boolean, hasEditingInMap: boolean): "edit
 };
 
 /** Euklidisk avstand mellom to koordinater i piksler */
-const pixelDistance = (coord1: Coordinate, coord2: Coordinate) => {
+export const pixelDistance = (coord1: Coordinate, coord2: Coordinate) => {
   const pixel1 = map.getPixelFromCoordinate(coord1);
   const pixel2 = map.getPixelFromCoordinate(coord2);
   const dx = pixel1[0] - pixel2[0];
