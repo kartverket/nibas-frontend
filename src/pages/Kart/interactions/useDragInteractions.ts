d55d9c5c660068b4c8fdac0195c5113ee38a24ac
plusk <andelar96@gmail.com>
2024-04-15 19:48:24 +0200

    Samler useCursorStyles så det er lettere å sikre riktig samspill

--- src/pages/Kart/interactions/useDragInteractions.ts
+++ src/pages/Kart/interactions/useDragInteractions.ts
@@ -1,27 +1,10 @@
 import { useToolbar } from "contexts/ToolbarContext";
-import { MapBrowserEvent } from "ol";
 import { shiftKeyOnly } from "ol/events/condition";
 import { DragPan, DragZoom } from "ol/interaction";
 import { useMemo } from "react";
-import { useCursorStyles } from "./useCursorStyles";
 
 const useDragInteractions = () => {
   const { activeModeTools } = useToolbar();
-
-  useCursorStyles({
-    isEnabled: activeModeTools.includes("move"),
-    defaultCursor: () => "grab",
-    eventsAndCursor: [
-      {
-        name: "pointerdrag",
-        cursor: (e) => (shiftKeyOnly(e as MapBrowserEvent<UIEvent>) ? "zoom-in" : "grabbing"),
-      },
-      {
-        name: "mouseup",
-        cursor: () => "grab",
-      },
-    ],
-  });
 
   const dragPan = useMemo(
     () =>
