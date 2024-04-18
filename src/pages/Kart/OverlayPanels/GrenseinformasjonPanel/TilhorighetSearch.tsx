0b3fdb848e0d3d76c730dff1afdcdd9ec7e740d3
Steffen Hageland <steffen.hageland@bekk.no>
2024-04-18 13:35:18 +0200

    TS-1669 Endrer tilhørighet dropdown til søk

---
+++ src/pages/Kart/OverlayPanels/GrenseinformasjonPanel/TilhorighetSearch.tsx
@@ -0,0 +1,91 @@
+import { Icon, InputProps, SearchAsync, Text } from "@kvib/react";
+import { FormatOptionLabelMeta } from "chakra-react-select";
+import { ReactNode, useMemo } from "react";
+import { styled } from "styled-components";
+
+type OptionType = { value: string; label: string };
+
+type Props = Omit<InputProps, "onChange"> & {
+  options: OptionType[];
+  onChange: (newId: string | undefined) => void;
+};
+
+const noOptionMessage = (obj: { inputValue: string }): ReactNode => {
+  return obj.inputValue !== "" ? (
+    <ErrorMessage>{`Fant ingen inndeling som matchet "${obj.inputValue}"`}</ErrorMessage>
+  ) : null;
+};
+
+export const highlightFormatter = (option: OptionType, formatOptionLabelMeta: FormatOptionLabelMeta<OptionType>) => {
+  // Sikre at input-verdi er escapet for bruk i RegExp
+  const escapedInputValue = formatOptionLabelMeta.inputValue
+    .replace(/[|\\{}()[\]^$+*?.]/g, "\\$&")
+    .replace(/-/g, "\\x2d");
+
+  const labelRegExp = new RegExp(`(.*)(${escapedInputValue})(.*)`, "i");
+  const matches = option.label.match(labelRegExp);
+
+  if (!matches)
+    return (
+      <OptionContainer>
+        <Text>{option.label}</Text>
+      </OptionContainer>
+    );
+
+  return (
+    <OptionContainer>
+      <Text>
+        {matches[1]}
+        <strong>{matches[2]}</strong>
+        {matches[3]}
+      </Text>
+    </OptionContainer>
+  );
+};
+
+export const TilhorighetSearch = ({ options, value, onChange }: Props) => {
+  const loadResults = async (term: string, resultsCallback: (options: OptionType[]) => void) => {
+    // Vi begrenser antall treff vi viser fordi enkelte grenser kan få VELDIG mange alternativer, spesielt
+    // kommunegrenser. Om ikke alternativet dukker opp må man prøve å søke.
+    if (term == null || term === "") {
+      resultsCallback(options.slice(0, 40));
+    } else {
+      resultsCallback(options.filter((option) => option.label.includes(term)).slice(0, 40));
+    }
+  };
+
+  const selectedValue = useMemo(() => {
+    return options.find((option) => option.value === value);
+  }, [options, value]);
+
+  return (
+    <SearchContainer>
+      <SearchAsync
+        optionLabelFormatter={highlightFormatter}
+        value={selectedValue}
+        placeholder="Skriv inn navnet eller nummeret til inndelingen"
+        dropdownIndicator={<Icon icon="expand_more" weight={400} />}
+        defaultOptions={true}
+        noOptionsMessage={noOptionMessage}
+        loadOptions={loadResults}
+        debounceTime={50}
+        onChange={(newValue) => onChange(newValue?.value)}
+      />
+    </SearchContainer>
+  );
+};
+
+const ErrorMessage = styled.div`
+  padding: 2px 16px;
+`;
+
+const OptionContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+const SearchContainer = styled.div`
+  background: var(--kvib-colors-white);
+`;
