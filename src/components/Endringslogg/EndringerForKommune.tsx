937ff1fdcd58d64a6939de7abcf2535877a80ef1
Steffen Hageland <steffen.hageland@kartverket.no>
2024-05-06 11:04:55 +0200

    TS-1352 Mer detaljert endringslogg (#582)
    En mer detaljert endringslog med nytt design som skal være litt mer
    oversiktlig. Jeg tror og det kan være greit å teste godt at ulike
    endringer man gjør faktisk dukker opp i endringsloggen.
    
    Jeg endte opp med å skrive om en del av koden for å hente ut endringer
    for å i større grad ha gjenbruk på tvers av grunnkrets og stemmekrets.
    Men jeg gikk ned et lite rabbithole med noen typescript-typer så jeg må
    innrømme jeg ikke er helt fornøyd med hvordan det ble. Men det ble godt
    nok, det fungerer - og med en liten dose sunk cost fallacy så ville jeg
    ikke skrive om en gang til :D
    
    En viktig detalj å få med seg er at ulagrede endringer bygger opp data
    basert på history - mens lagrede endringer er bygd opp basert på
    Utkastet, disse har litt ulike data tilgjengelig. Så å vite hva som er
    endret på en grense er litt knot siden man da må hente grensen fra
    backend og sammenligne alle feltene på featuren. Har laget egen oppgave
    som går på å utvide med denne funksjonaliteten.
    
    <img width="1304" alt="image"
    src="https://github.com/kartverket/nibas-frontend/assets/2163425/d6eae78b-0201-45d8-b75c-cacf9bfe3fa2">
    
    ---------
    
    Co-authored-by: William Andersson <wandersson.98@gmail.com>

---
+++ src/components/Endringslogg/EndringerForKommune.tsx
@@ -0,0 +1,72 @@
+import { Heading } from "@kvib/react";
+import { Kretsendringer, Metadataendringer } from "components/Endringslogg/hooks/utkastEndringerTypes";
+import { EndringNumericCard, EndringToFromCard } from "components/Endringslogg/Endringcard/EndringCard";
+import { EndringListItem } from "components/Endringslogg/EndringerList";
+import { styled } from "styled-components";
+import { getNavnOgNummerChanges, getValgdistriktChanges } from "components/Endringslogg/Endringcard/EndringCardUtils";
+import { KontekstType } from "pages/Kart/OverlayPanels/hooks/tilhorighet-utils";
+
+type EndringerForKommuneProps = {
+  kretstype: KontekstType;
+  endringer: Kretsendringer<Metadataendringer>;
+};
+
+const harEndringer = (endringer: Kretsendringer<Metadataendringer>) =>
+  endringer.antallArkiverteGrenser > 0 ||
+  endringer.antallNyeGrenser > 0 ||
+  endringer.antallEndredeGrenser > 0 ||
+  (endringer.delinger?.length ?? 0) !== 0 ||
+  endringer.sammenslaaing != null ||
+  endringer.metadataendringer.length > 0;
+
+export const EndringerForKommune = ({ kretstype, endringer }: EndringerForKommuneProps) => {
+  const titlePrefix = kretstype === "STEMMEKRETS" ? "Stemmekretsendringer" : "Grunnkretsendringer";
+  const { metadataendringer, antallArkiverteGrenser, antallEndredeGrenser, antallNyeGrenser, delinger, sammenslaaing } =
+    endringer;
+
+  if (!harEndringer(endringer)) {
+    return null;
+  }
+
+  const navnOgNummerChanges = getNavnOgNummerChanges(metadataendringer);
+  const valgdistriktEndringer = kretstype === "STEMMEKRETS" ? getValgdistriktChanges(metadataendringer) : [];
+
+  return (
+    <EndringListItem>
+      <KommuneHeading as="h3">
+        {titlePrefix} i {endringer.kommune.nummer} {endringer.kommune.navn}
+      </KommuneHeading>
+      <EndringToFromCard type="flatedetaljer" changes={navnOgNummerChanges} />
+      <EndringToFromCard type="valgdistrikt" changes={valgdistriktEndringer} />
+      {sammenslaaing != null && (
+        <EndringToFromCard
+          type="sammenslåing"
+          changes={[
+            {
+              from: sammenslaaing.gamleKretser.map(({ navn, nummer }) => `${nummer} ${navn}`),
+              to: [`${sammenslaaing.nyttNummer} ${sammenslaaing.nyttNavn}`],
+            },
+          ]}
+        />
+      )}
+      {delinger != null && (
+        <EndringToFromCard
+          type="deling"
+          changes={delinger.map((deling) => ({
+            from: [`${deling.opprinneligKrets.kretsNummer} ${deling.opprinneligKrets.kretsNavn}`],
+            to: deling.nyeKretser.map((krets) => `${krets.kretsNummer} ${krets.kretsNavn}`),
+          }))}
+        />
+      )}
+      <EndringNumericCard type="grenseendring" value={antallEndredeGrenser} />
+      <EndringNumericCard type="nyegrenser" value={antallNyeGrenser} />
+      <EndringNumericCard type="arkiveringer" value={antallArkiverteGrenser} />
+    </EndringListItem>
+  );
+};
+
+const KommuneHeading = styled(Heading)`
+  font-size: var(--kvib-fontSizes-xl);
+  font-weight: 900;
+  margin-bottom: 1rem;
+`;
