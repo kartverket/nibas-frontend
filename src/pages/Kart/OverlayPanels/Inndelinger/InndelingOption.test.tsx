99039d76cfd7cfca05394f4cd1e3d976fb5cf3e9
anderssonw <wandersson.98@gmail.com>
2024-05-21 11:51:03 +0200

    use container and queryselecter instead for tests
    Signed-off-by: anderssonw <wandersson.98@gmail.com>

--- src/pages/Kart/OverlayPanels/Inndelinger/InndelingOption.test.tsx
+++ src/pages/Kart/OverlayPanels/Inndelinger/InndelingOption.test.tsx
@@ -4,45 +4,42 @@ import InndelingOption from "./InndelingOption";
 
 describe("InndelingOption", () => {
   it("should be a radio input if radio prop is sent", () => {
-    const { queryByTestId } = render(
+    const { container } = render(
       <InndelingOption isActive={false} onClick={() => {}} type="radio">
         <>Test</>
       </InndelingOption>,
     );
 
-    expect(queryByTestId("radio_input")).toBeTruthy();
-    expect(queryByTestId("checkbox_input")).toBeNull();
+    expect(container.querySelector("input")?.type).toEqual("radio");
   });
 
   it("should be a checkbox input if checkbox prop is sent", () => {
-    const { queryByTestId } = render(
+    const { container } = render(
       <InndelingOption isActive={false} onClick={() => {}} type="checkbox">
         <>Test</>
       </InndelingOption>,
     );
 
-    expect(queryByTestId("checkbox_input")).toBeTruthy();
-    expect(queryByTestId("radio_input")).toBeNull();
+    expect(container.querySelector("input")?.type).toEqual("checkbox");
   });
 
   it("should not have an input if button prop is sent", () => {
-    const { queryByTestId } = render(
+    const { container } = render(
       <InndelingOption isActive={false} onClick={() => {}} type="button">
         <>Test</>
       </InndelingOption>,
     );
 
-    expect(queryByTestId("checkbox_input")).toBeNull();
-    expect(queryByTestId("radio_input")).toBeNull();
+    expect(container.querySelector("input")?.type).toBeUndefined();
   });
 
   it("should have a checked input field if isActive is true", () => {
-    const { getByRole } = render(
+    const { container } = render(
       <InndelingOption isActive={true} onClick={() => {}} type="checkbox">
         <>Test</>
       </InndelingOption>,
     );
 
-    expect(getByRole("checkbox", { checked: true })).toBeTruthy();
+    expect(container.querySelector("input")?.checked).toBeTruthy();
   });
 });
