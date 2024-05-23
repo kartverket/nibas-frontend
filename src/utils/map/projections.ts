41a7c6512bd98ce34180f1a471da5c607c4e6385
erlendst <erlend.stolan@kartverket.no>
2024-05-23 20:27:39 +0200

    Endrer rekkefølge på projections

--- src/utils/map/projections.ts
+++ src/utils/map/projections.ts
@@ -26,20 +26,6 @@ export const projectionDefinitions: EpsgDefinition[] = [
     xyLabel: { x: "Øst", y: "Nord" },
   },
   {
-    name: "EUREF89 - Geografisk, grader (Lat/Lon)",
-    shortName: "EU89 Geografisk, grader",
-    epsgCode: "EPSG:4258",
-    def: "+proj=longlat +ellps=GRS80 +no_defs +type=crs",
-    xyLabel: { x: "Lengdegrad", y: "Breddegrad" },
-  },
-  {
-    name: "WGS84 (Google Maps)",
-    shortName: "WGS84 Google",
-    epsgCode: "EPSG:4326",
-    def: "+proj=longlat +datum=WGS84 +no_defs +type=crs",
-    xyLabel: { x: "E (Øst)", y: "N (Nord)" },
-  },
-  {
     name: "EUREF89 - UTM-sone 32",
     shortName: "EU89 UTM32",
     epsgCode: "EPSG:25832",
@@ -52,6 +38,20 @@ export const projectionDefinitions: EpsgDefinition[] = [
     epsgCode: "EPSG:25835",
     def: "+proj=utm +zone=35 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs",
     xyLabel: { x: "Øst", y: "Nord" },
+  },
+  {
+    name: "EUREF89 - Geografisk, grader (Lat/Lon)",
+    shortName: "EU89 Geografisk, grader",
+    epsgCode: "EPSG:4258",
+    def: "+proj=longlat +ellps=GRS80 +no_defs +type=crs",
+    xyLabel: { x: "Lengdegrad", y: "Breddegrad" },
+  },
+  {
+    name: "WGS84 (Google Maps)",
+    shortName: "WGS84 Google",
+    epsgCode: "EPSG:4326",
+    def: "+proj=longlat +datum=WGS84 +no_defs +type=crs",
+    xyLabel: { x: "E (Øst)", y: "N (Nord)" },
   },
 ];
 
