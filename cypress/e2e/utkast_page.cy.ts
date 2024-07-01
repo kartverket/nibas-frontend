abf8c255b452062ed7035f7a7e5cdc50ed5c50d9
Aleksander Karlsen <aleweskar@gmail.com>
2024-07-01 12:25:44 +0200

    Add test

--- cypress/e2e/utkast_page.cy.ts
+++ cypress/e2e/utkast_page.cy.ts
@@ -1,11 +1,61 @@
 describe("kart page", () => {
-  it("should toggle kommune", () => {
+  it("should open stemmekrets, remove point, add point, splitt grense, draw grense, splitt flate, and set tilhørigheter, save and publish", () => {
     cy.login();
     cy.visit("/utkast");
+
+    cy.setupTestingGlobal("map");
+
     cy.createUtkast();
     cy.wait(1000);
-    cy.toggleRedigerInndeling("Kommune", "32 Akershus", "Frogn");
-    cy.wait(10000);
-    cy.deleteUtkast();
+    const inndelingsType = "Stemmekrets";
+    cy.toggleRedigerInndeling(inndelingsType, "Akershus", "Frogn");
+    cy.wait(6000);
+
+    cy.get("@map").then((map) => {
+      cy.toggleTool("remove");
+      // klikker på grensen for å velge å redigere den
+      cy.clickAtCoordinate(map, [251343, 6627657]);
+      // Klikker på punkt på nytt for å fjerne punkt
+      cy.clickAtCoordinate(map, [251343, 6627657]);
+
+      cy.toggleTool("add");
+      // klikker på grensen for å velge å redigere den
+      cy.clickAtCoordinate(map, [251343, 6627657]);
+      // Klikker på punkt på nytt for å legge til punkt
+      cy.clickAtCoordinate(map, [251343, 6627657]);
+
+      cy.toggleTool("split");
+      // klikker på grensen sitt punkt for å velge å splitte den på punktet
+      cy.clickAtCoordinate(map, [251343, 6627657]);
+      cy.contains("Del grense").click();
+
+      const nyGrenseCoordinates = [
+        [251343, 6627657],
+        [253854.9, 6627663.16],
+        [257099.81, 6627826.09],
+      ] as [number, number][];
+      cy.drawGrense(map, nyGrenseCoordinates);
+
+      const nyFlate = { navn: "Testsplitt", nummer: "10" };
+      cy.splittFlate("07 Nordre Frogn", [nyFlate]);
+
+      cy.settTilhorighetNyGrense(map, nyGrenseCoordinates[1], "Stemmekretser", [
+        { navn: "Testsplitt", nummer: "10" },
+        { navn: "Nordre Frogn", nummer: "07" },
+      ]);
+
+      cy.settTilhorighetEksisterendeGrense(map, [255274.61, 6627364.6], "Stemmekretser", [
+        { fra: "07 Nordre Frogn", til: "10 Testsplitt" },
+        { fra: "03 Heer", til: "03 Heer" },
+      ]);
+
+      cy.settTilhorighetEksisterendeGrense(map, [251748, 6626277.99], "Stemmekretser", [
+        { fra: "07 Nordre Frogn", til: "10 Testsplitt" },
+        { fra: "01 Drøbak", til: "01 Drøbak" },
+      ]);
+    });
+
+    cy.saveUtkast();
+    cy.publiserUtkast();
   });
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,17 +1,67 @@
 /// <reference types="cypress" />
+
 /* eslint-disable @typescript-eslint/no-namespace */
+
+type InndelingsType = "Fylke" | "Kommune" | "Stemmekrets" | "Grunnkrets";
+
+const toolValues = ["add", "remove", "draw", "split", "grenseinfo", "koordinater", "archive"] as const;
+const modeToolValues = ["move", "matrikkel", "snap_nibas", "snap_matrikkel"] as const;
+const sidepanelValues = ["splitt", "sammenslaaing", "kartlag"] as const;
+type Tool = (typeof toolValues)[number];
+type ModeTool = (typeof modeToolValues)[number];
+type Sidepanel = (typeof sidepanelValues)[number];
 
 declare namespace Cypress {
   interface Chainable {
+    setupTestingGlobal: (globalKey: string) => void;
     login: () => void;
     createUtkast: () => void;
+    saveUtkast: () => void;
+    publiserUtkast: () => void;
     deleteUtkast: () => void;
-    toggleRedigerInndeling: (inndelingsType: string, fylke: string, kommuner: string) => void;
-    simulateOpenLayersEvent: (ol, map, type, x, y, opt_shiftKey) => void;
+    toggleRedigerInndeling: (inndelingsType: InndelingsType, fylke: string, kommune: string) => void;
+    toggleTool: (tool: Tool) => void;
+    toggleModeTool: (tool: ModeTool) => void;
+    toggleSidePanel: (sidepanel: Sidepanel) => void;
+    escape: () => void;
+    clickAtCoordinate: (map, coordinate: number[], doubleClick?: boolean) => Chainable<JQuery<HTMLCanvasElement>>;
+    drawGrense: (map, coordinates: [number, number][]) => void;
+    splittFlate: (opprinneligFlateName: string, nyeFlater: { navn: string; nummer: string }[]) => void;
+    settTilhorighetNyGrense: (
+      map,
+      grensepunkt: number[],
+      inndelingstype: "Stemmekretser" | "Grunnkretser",
+      flater: [{ navn: string; nummer: string }, { navn: string; nummer: string }],
+    ) => void;
+    settTilhorighetEksisterendeGrense: (
+      map,
+      grensepunkt: number[],
+      inndelingstype: "Stemmekretser" | "Grunnkretser",
+      fraTil: [{ fra: string; til: string }, { fra: string; til: string }],
+    ) => void;
+    clickOnFeature: (map, layer: string, id: string, coordinateIndex: number) => object;
   }
 }
 
 const baseUrl = Cypress.env("baseUrl") ?? "http://localhost:3000";
+
+// Sjekker om det finnes en global testing variable med navn lik globalKey, og setter denne med samme navn hvis den finnes, ellers venter den 100ms og sjekker på nytt.
+Cypress.Commands.add("setupTestingGlobal", (globalKey) => {
+  cy.window()
+    .then((win) => {
+      return new Cypress.Promise((resolve) => {
+        const checkGlobal = () => {
+          if (win.testingGlobals?.[globalKey] != null) {
+            resolve(win.testingGlobals[globalKey]);
+          } else {
+            setTimeout(checkGlobal, 100);
+          }
+        };
+        checkGlobal();
+      });
+    })
+    .as(`${globalKey}`);
+});
 
 Cypress.Commands.add("login", () => {
   cy.intercept("GET", `${baseUrl}/v1/authz/status`, (req) => {
@@ -56,18 +106,214 @@ Cypress.Commands.add("createUtkast", () => {
 });
 
 Cypress.Commands.add("deleteUtkast", () => {
-  //Sjekker om utkastet man prøver å slette har samme navn som createUtkast-metoden sitt utkast
-  cy.contains(utkastName);
   cy.contains("Slett utkast").click();
   cy.get(".chakra-modal__footer").contains("Slett utkast").click();
 });
 
-// Cypress.Commands.add("simulateOpenLayersEvent", (ol, map, type, x, y, opt_shiftKey = undefined) => {
-//   const viewport = map.getViewport();
-//   const position = viewport.getBoundingClientRect();
-//   cy.log(`left: ${position.left}, top: ${position.top}, width: ${position.width}, height: ${position.height}`);
-//   cy.get("canvas").trigger(type, {
-//     clientX: position.left + x + position.width / 2,
-//     clientY: position.top + y + position.height / 2,
-//   });
-// });
+Cypress.Commands.add("toggleTool", (tool) => {
+  switch (tool) {
+    case "add": {
+      cy.get("body").type("{ctrl}L");
+      break;
+    }
+    case "remove": {
+      cy.get("body").type("{ctrl}{shift}L");
+      break;
+    }
+    case "draw": {
+      cy.get("body").type("{ctrl}T");
+      break;
+    }
+    case "split": {
+      cy.get("body").type("{ctrl}{shift}P");
+      break;
+    }
+    case "grenseinfo": {
+      cy.get("body").type("{ctrl}I");
+      break;
+    }
+    case "koordinater": {
+      cy.get("body").type("{ctrl}F");
+      break;
+    }
+    case "archive": {
+      cy.get("body").type("{ctrl}A");
+      break;
+    }
+  }
+});
+
+Cypress.Commands.add("toggleModeTool", (modeTool) => {
+  switch (modeTool) {
+    case "move": {
+      cy.get("body").type("V");
+      break;
+    }
+    case "matrikkel": {
+      cy.get("body").type("{ctrl}E");
+      break;
+    }
+    case "snap_nibas": {
+      cy.get("body").type("{ctrl}G");
+      cy.contains("Snap til egne grenser").click();
+      break;
+    }
+    case "snap_matrikkel": {
+      cy.get("body").type("{ctrl}G");
+      cy.contains("Snap til teiggrenser").click();
+      break;
+    }
+  }
+});
+
+Cypress.Commands.add("toggleSidePanel", (sidepanel) => {
+  switch (sidepanel) {
+    case "splitt": {
+      cy.get("body").type("{ctrl}{shift}M");
+      break;
+    }
+    case "sammenslaaing": {
+      cy.get("body").type("{ctrl}M");
+      break;
+    }
+    case "kartlag": {
+      cy.get("body").type("{ctrl}K");
+      break;
+    }
+  }
+});
+
+Cypress.Commands.add("escape", () => {
+  cy.get("body").type("{esc}");
+});
+
+Cypress.Commands.add("clickAtCoordinate", (map, coordinate, doubleClick = false) => {
+  const featurePointPixelValue = map.getPixelFromCoordinate(coordinate);
+  if (doubleClick === true) {
+    return cy.get("canvas").dblclick(featurePointPixelValue[0], featurePointPixelValue[1]);
+  } else {
+    return cy.get("canvas").click(featurePointPixelValue[0], featurePointPixelValue[1]);
+  }
+});
+
+Cypress.Commands.add("saveUtkast", () => {
+  cy.get("body").type("{ctrl}S");
+});
+
+Cypress.Commands.add("publiserUtkast", () => {
+  cy.contains("Publiser utkast").click();
+  cy.get(".chakra-modal__footer").contains("Publiser utkast").click();
+});
+
+Cypress.Commands.add("drawGrense", (map, coordinates) => {
+  cy.toggleTool("draw");
+  coordinates.forEach((coordinate, i) => {
+    cy.wait(250); // Wait for å kunne se at grensa tegnes
+    if (i > 0) {
+      cy.get(`@drawGrensepunkt-${i - 1}`).then(() => {
+        if (i === coordinates.length - 1) {
+          cy.clickAtCoordinate(map, coordinate, true).as(`drawGrensepunkt-${i}`);
+        } else {
+          cy.clickAtCoordinate(map, coordinate).as(`drawGrensepunkt-${i}`);
+        }
+      });
+    } else {
+      cy.clickAtCoordinate(map, coordinate).as(`drawGrensepunkt-${i}`);
+    }
+  });
+  cy.get(`@drawGrensepunkt-${coordinates.length - 1}`).then(() => {
+    // lukker grenseinfopanel etter opprettng av ny grense
+    cy.escape();
+    // lukker tegning
+    cy.toggleTool("draw");
+  });
+});
+
+Cypress.Commands.add("splittFlate", (opprinneligFlateName, nyeFlater) => {
+  cy.toggleSidePanel("splitt");
+  cy.get("select").select(opprinneligFlateName);
+
+  nyeFlater.forEach((nyFlate, i) => {
+    const fillOutNyFlateInputs = () => {
+      cy.contains("button", "Legg til ny splitt").click();
+      cy.get("label")
+        .filter(":contains(Nytt nummer)")
+        .last()
+        .siblings()
+        .type(nyFlate.nummer)
+        .then(() => {
+          cy.get("label").filter(":contains(Nytt navn)").last().siblings().type(nyFlate.navn);
+        })
+        .as(`split-${i}`);
+    };
+    if (i > 0) {
+      cy.get(`@split-${i - 1}`).then(fillOutNyFlateInputs);
+    } else {
+      fillOutNyFlateInputs();
+    }
+  });
+
+  cy.get(`@split-${nyeFlater.length - 1}`).then(() => {
+    cy.contains("button", "Splitt").click();
+    cy.escape();
+  });
+});
+
+Cypress.Commands.add("settTilhorighetNyGrense", (map, grensepunkt, inndelingsType, flater) => {
+  cy.toggleTool("grenseinfo");
+  cy.clickAtCoordinate(map, grensepunkt);
+  cy.contains("Tilhørighet").parent().contains("Rediger").click();
+  cy.contains(inndelingsType)
+    .parent()
+    .parent()
+    .siblings()
+    .within(() => {
+      const flate1 = flater[0];
+      cy.contains("Skriv inn navnet eller nummeret til kretsen").click({ force: true });
+      cy.wait(200);
+      cy.contains(`${flate1.nummer} ${flate1.navn}`).dblclick({ force: true });
+
+      cy.wait(500);
+      const flate2 = flater[1];
+      cy.contains("Skriv inn navnet eller nummeret til kretsen").click({ force: true });
+      cy.wait(200);
+      cy.contains(`${flate2.nummer} ${flate2.navn}`).dblclick({ force: true });
+    });
+  cy.contains("Bekreft").click();
+  cy.escape();
+  cy.toggleTool("grenseinfo");
+});
+
+Cypress.Commands.add("settTilhorighetEksisterendeGrense", (map, grensepunkt, inndelingsType, fraTil) => {
+  cy.toggleTool("grenseinfo");
+  cy.clickAtCoordinate(map, grensepunkt);
+  cy.contains("Tilhørighet").parent().contains("Rediger").click();
+  cy.contains(inndelingsType)
+    .parent()
+    .parent()
+    .siblings()
+    .within(() => {
+      const bytte1 = fraTil[0];
+      cy.contains(bytte1.fra).click({ force: true });
+      cy.wait(200);
+      cy.contains(`${bytte1.til}`).dblclick({ force: true });
+
+      cy.wait(500);
+      const bytte2 = fraTil[1];
+      cy.contains(bytte2.fra).click({ force: true });
+      cy.wait(200);
+      cy.contains(`${bytte2.til}`).dblclick({ force: true });
+    });
+  cy.contains("Bekreft").click();
+  cy.escape();
+  cy.toggleTool("grenseinfo");
+});
+
+Cypress.Commands.add("clickOnFeature", (map, layer, id, coordinateIndex) => {
+  const layers = map.getAllLayers();
+  const editLayer = layers.find((l) => l.get("id") === layer);
+  const editSource = editLayer.getSource();
+  const feature = editSource.getFeatureById(id);
+  const coordinates = feature.getGeometry().getCoordinates();
+  cy.clickAtCoordinate(map, coordinates[coordinateIndex]);
+});
