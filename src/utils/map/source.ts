f1b5ffb9faf37be13a93f5f0c2026cab9e2853ba
Kristian Marison Haugerud <kristian.haugerud@kartverket.no>
2025-02-13 14:07:36 +0100

    TS-2054: Refactor til VectorSource

--- src/utils/map/source.ts
+++ src/utils/map/source.ts
@@ -37,7 +37,7 @@ const setSharedIndexIfFeatureInSource = (source: VectorSource, featureId: string
 };
 
 export const addFeaturesToSource = (sourceId: LayerId, features: Feature<Geometry>[], callback?: () => void) => {
-  const layer = getLayerById(sourceId) as VectorLayer<Feature>;
+  const layer = getLayerById(sourceId) as VectorLayer<VectorSource<Feature<Geometry>>>;
   const source = layer.getSource();
   if (!source) {
     return;
@@ -70,7 +70,7 @@ export const addFeaturesToSource = (sourceId: LayerId, features: Feature<Geometr
 };
 
 export const removeFeaturesFromSourceByIds = (sourceId: LayerId, featureIds: string[]) => {
-  const layer = getLayerById(sourceId) as VectorLayer<Feature>;
+  const layer = getLayerById(sourceId) as VectorLayer<VectorSource<Feature<Geometry>>>;
   const source = layer.getSource();
   if (!source) {
     return;
