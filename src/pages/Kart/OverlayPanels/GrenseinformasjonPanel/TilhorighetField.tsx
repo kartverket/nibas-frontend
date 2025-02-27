38ed7654f1e47d450b51502aa71e90e578b33463
Aleksander Karlsen <aleweskar@gmail.com>
2025-02-27 10:56:29 +0100

    Revert "TS-2019: Lagt til deaktivering tilhørighet (#721)"
    This reverts commit 7dc347414874dea0589543438140e8f37382a893.

--- src/pages/Kart/OverlayPanels/GrenseinformasjonPanel/TilhorighetField.tsx
+++ src/pages/Kart/OverlayPanels/GrenseinformasjonPanel/TilhorighetField.tsx
@@ -244,14 +244,13 @@ const KommunegrenseTilhorighetField = ({ feature, isDisabled }: TilhorighetProps
   );
 };
 
-const IkkeRedigerbarAdministrativGrense = ({ feature, isDisabled }: TilhorighetProps) => {
+const IkkeRedigerbarAdministrativGrense = ({ feature }: TilhorighetProps) => {
   const useTilhorighetGrunnkrets = useTilhorighetIkkeRedigerbar(feature, KontekstType.GRUNNKRETS);
   const useTilhorighetStemmekrets = useTilhorighetIkkeRedigerbar(feature, KontekstType.STEMMEKRETS);
 
   return (
     <TilhorighetFieldController
       feature={feature}
-      isDisabled={isDisabled}
       grunnkretsTilhorighetForm={useTilhorighetGrunnkrets}
       stemmekretsTilhorighetForm={useTilhorighetStemmekrets}
       renderChildren={({ isEditing, isSubmitted, isGrunnkretserValid, isStemmekretserValid }) => (
@@ -290,7 +289,7 @@ export const TilhorighetField = ({ feature, isDisabled = false }: TilhorighetPro
 
     return <KommunegrenseTilhorighetField feature={feature} isDisabled={shouldBeDisabled || !isEditable} />;
   } else if (isGrenseType(featureType) && isAdministrativGrense(featureType)) {
-    return <IkkeRedigerbarAdministrativGrense feature={feature} isDisabled={shouldBeDisabled} />;
+    return <IkkeRedigerbarAdministrativGrense feature={feature} />;
   }
 
   return <CommonTilhorighetField feature={feature} isDisabled={shouldBeDisabled} />;
