669b801d3af99adf2114cc79226831ffc3a0aa5c
Aleksander Karlsen <aleweskar@gmail.com>
2025-03-04 10:46:57 +0100

    Småplukk

--- src/pages/Kart/OverlayPanels/hooks/useAdministrativTilhorighet.ts
+++ src/pages/Kart/OverlayPanels/hooks/useAdministrativTilhorighet.ts
@@ -6,11 +6,45 @@ import {
   CustomOption,
   getKommunerIdFromKontekstEgenskaper,
   KontekstType,
+  mapGrunnkretsResponseToKrets,
+  mapStemmekretResponseToKrets,
   Tilhorighet,
   UseTilhorighet,
 } from "./tilhorighet-utils";
-import { useGetMuligeKretserForAdministrativGrense } from "./useGetMuligeKretserForAdministrativGrense";
 import { useTilhorighetForm } from "./useTilhorighetForm";
+import { useValgtGyldighetsdato } from "../../../../contexts/GyldighetsdatoContext";
+import useNibasApi from "../../../../hooks/useNibasApi";
+
+const useGetMuligeKretserForAdministrativGrense = (
+  kontekstType: KontekstType,
+  kommuneId: string | null | undefined,
+) => {
+  const { gyldighetsdato } = useValgtGyldighetsdato();
+  // for fylker er problemet at samme fylkeId blir brukt for alternativ A og B lista, mens vi må hente fra.
+  const urlForKrets =
+    kontekstType === KontekstType.GRUNNKRETS ? "/v1/kommuner/{id}/grunnkretser" : "/v1/kommuner/{id}/stemmekretser";
+  const url = kommuneId != null ? urlForKrets : null;
+
+  const { data, isLoading } = useNibasApi(url, { id: kommuneId!, gyldighetsdato });
+
+  const kretserForFylket = useMemo(() => {
+    if (data == null) {
+      return [];
+    }
+
+    switch (kontekstType) {
+      case KontekstType.STEMMEKRETS:
+        return mapStemmekretResponseToKrets(data);
+      case KontekstType.GRUNNKRETS:
+        return mapGrunnkretsResponseToKrets(data);
+    }
+  }, [kontekstType, data]);
+
+  return {
+    muligeKretser: kretserForFylket,
+    isLoading,
+  };
+};
 
 export const useAdministrativTilhorighet = (feature: Feature, kontekstType: KontekstType): UseTilhorighet => {
   const {
@@ -44,13 +78,7 @@ export const useAdministrativTilhorighet = (feature: Feature, kontekstType: Kont
     kommunerId[1],
   );
 
-  const muligeKretser = useMemo(
-    () =>
-      muligeKretserA
-        .concat(muligeKretserB)
-        .toSorted((kretsA, kretsB) => Number(kretsA.kommunenummer) - Number(kretsB.kommunenummer)),
-    [muligeKretserA, muligeKretserB],
-  );
+  const muligeKretser = useMemo(() => muligeKretserA.concat(muligeKretserB), [muligeKretserA, muligeKretserB]);
 
   useEffect(() => {
     setTilhorighetOptions({
