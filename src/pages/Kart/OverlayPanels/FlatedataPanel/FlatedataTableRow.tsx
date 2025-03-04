17acb2b01542844826494b0b68cca53b84c1b8a8
Aleksander Karlsen <aleweskar@gmail.com>
2025-03-04 16:32:25 +0100

    Add mock fremtidig informasjon

--- src/pages/Kart/OverlayPanels/FlatedataPanel/FlatedataTableRow.tsx
+++ src/pages/Kart/OverlayPanels/FlatedataPanel/FlatedataTableRow.tsx
@@ -55,6 +55,7 @@ type Props = {
   formMethods: UseFormReturn<FlatedataInputs>;
   previousValues: React.MutableRefObject<FlatedataInputs | undefined>;
   allInndelinger: MetadataResponse[];
+  mockSammenslaaingInformasjon: string | undefined;
 };
 
 export const FlatedataTableRow = ({
@@ -65,6 +66,7 @@ export const FlatedataTableRow = ({
   formMethods,
   previousValues,
   allInndelinger,
+  mockSammenslaaingInformasjon,
 }: Props) => {
   const {
     setValue,
@@ -258,7 +260,7 @@ export const FlatedataTableRow = ({
           <InputCell
             isEditing={isEditing}
             isDisabled={disabledDate != null}
-            data={getValues(`${inndelingId}.informasjon`) ?? inndeling.informasjon}
+            data={mockSammenslaaingInformasjon ?? getValues(`${inndelingId}.informasjon`) ?? inndeling.informasjon}
             {...register(`${inndelingId}.informasjon`)}
           />
           {!isKommuneInndeling(inndeling) && !isStemmekretsInndeling(inndeling) && (
