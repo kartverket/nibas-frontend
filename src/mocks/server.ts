b8ac5559469ad246cd2fa76041f11c9f66f0ae2e
JonathanRosby <jonathan.rosby@kartverket.no>
2025-05-09 10:18:14 +0200

    suppress TS handler brand mismatch with ts-expect-error

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -2,4 +2,5 @@ import { setupServer } from "msw/node";
 import { handlers } from "./handlers/handlers";
 
 // This configures a request mocking server with the given request handlers.
+// @ts-expect-error workaround for MSW private __kind mismatch
 export const server = setupServer(...handlers);
