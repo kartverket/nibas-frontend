b60d382d118b1edeac963d3b0ef8075665097e84
JonathanRosby <jonathan.rosby@kartverket.no>
2025-05-09 13:09:47 +0200

    fix

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -2,5 +2,4 @@ import { setupServer } from "msw/node";
 import { handlers } from "./handlers/handlers";
 
 // This configures a request mocking server with the given request handlers.
-// @ts-expect-error workaround for MSW private __kind mismatch
 export const server = setupServer(...handlers);
