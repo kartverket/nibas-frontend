7e2818fe64b1cd080b32edaf8494a00a4a0e00ed
Aleksander Karlsen <aleweskar@gmail.com>
2025-05-28 12:49:29 +0200

    Add halfup rounding

--- src/pages/Kart/OverlayPanels/FlyttKoordinaterPanel.tsx
+++ src/pages/Kart/OverlayPanels/FlyttKoordinaterPanel.tsx
@@ -24,6 +24,7 @@ import {
   transformCoordinatesToProjection,
 } from "./NavigasjonPanel/koordinater-utils";
 import { PanelHeader, SidePanel } from "./Panel";
+import Decimal from "decimal.js";
 
 type KoordinaterFormData = {
   north: number;
@@ -225,9 +226,12 @@ const FlyttKoordinaterPanel = () => {
   const { data: nasjon, isLoading, error: nasjonFetchError } = useNibasApi("/v1/nasjon/", { gyldighetsdato });
   const [globalFormError, setGlobalFormError] = useState<string | null>();
 
+  const roundHalfUp = (value: number): number =>
+    new Decimal(value).toDecimalPlaces(2, Decimal.ROUND_HALF_UP).toNumber();
+
   const movePointToCoordinates = () => {
     setGlobalFormError(null);
-    const [east, north] = [getValues("east"), getValues("north")];
+    const [east, north] = [roundHalfUp(getValues("east")), roundHalfUp(getValues("north"))];
     const transformedCoordinates = transformCoordinatesToProjection(
       east,
       north,
