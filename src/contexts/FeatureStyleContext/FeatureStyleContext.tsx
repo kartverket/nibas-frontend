3743b9ee64a153c18f8295906ba10cbd9e36d3fc
Aleksander Karlsen <aleweskar@gmail.com>
2025-08-01 14:51:46 +0200

    Stupid fix

--- src/contexts/FeatureStyleContext/FeatureStyleContext.tsx
+++ src/contexts/FeatureStyleContext/FeatureStyleContext.tsx
@@ -11,6 +11,7 @@ import {
   getAllFeatureEndPointCoordinates,
   getFeaturesConnectedToFeatureAtEndpoints,
   isFeatureDeadEnd,
+  isFeatureToBeArchived,
 } from "utils/features";
 import { HistoryEntry, HistoryTypeValues } from "contexts/HistoryContext/types";
 import { filterOnlyDeadEnds, mapAffectedFeaturesForErrorEntries } from "./feature-style-utils";
@@ -75,7 +76,14 @@ export const FeatureStyleProvider = ({ children }: { children: React.ReactNode }
         customStyle.savedCustomFeatureIds.includes(featureId),
       );
 
-      if (matchingCustomStyle) {
+      // TS-2288: updateFeatureStyles blir kalt når en history entry legges til i history som en del av arkiveringen.
+      // Det aktuelle objektet finnes i listen archivedFeatures i updateFeatureStyles, og setCustomStyles for archivedStyleFunctions blir kalt med dette objektet.
+      // Senere i arkiveringsprosessen, når handlingen ryddes opp, blir objektet deselected ved å bruke clearSelection, som igjen utløser setDeselectStyle.
+      // I denne setDeselectStyle finnes ikke featureId i customStyles for archivedStyleFunctions, noe som fører til at feil stil blir satt på objektet til slutt.
+      // Vet ikke hvorfor dette skjer, så her er en eksplisitt sjekk på featuren for å sikre at den får riktig stil.
+      if (isFeatureToBeArchived(feature)) {
+        feature.setStyle(archivedStyleFunctions.customStyle);
+      } else if (matchingCustomStyle) {
         feature.setStyle(matchingCustomStyle.customStyle);
       } else if (matchingSavedCustomStyle) {
         feature.setStyle(matchingSavedCustomStyle.customStyle);
