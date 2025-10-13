f7933b11eaf3e917925b51cf179066a73b49f193
Kristian Marison Haugerud <kristian.haugerud@kartverket.no>
2025-10-13 06:23:06 +0200

    fix lint i generert fil...

--- src/types/api-gen.ts
+++ src/types/api-gen.ts
@@ -505,13 +505,7 @@ export interface components {
        * @description Flatetypen som skal deles
        * @enum {string}
        */
-      flatetype:
-        | "FYLKE"
-        | "KOMMUNE"
-        | "NASJON"
-        | "GRUNNKRETS"
-        | "STEMMEKRETS"
-        | "SKOLEKRETS";
+      flatetype: "FYLKE" | "KOMMUNE" | "NASJON" | "GRUNNKRETS" | "STEMMEKRETS" | "SKOLEKRETS";
       /** @description Navn og nummer for de nye kretsene som skal utledes fra opprinnelig krets */
       nyeKretser: components["schemas"]["KretsNavnOgNummer"][];
     };
@@ -1031,13 +1025,7 @@ export interface components {
        * @description Flatetypen til inndelingen
        * @enum {string}
        */
-      type:
-        | "FYLKE"
-        | "KOMMUNE"
-        | "NASJON"
-        | "GRUNNKRETS"
-        | "STEMMEKRETS"
-        | "SKOLEKRETS";
+      type: "FYLKE" | "KOMMUNE" | "NASJON" | "GRUNNKRETS" | "STEMMEKRETS" | "SKOLEKRETS";
       /** @description Navnet til inndelingen */
       navn: string;
       /** @description Nummeret til inndelingen */
@@ -1119,13 +1107,7 @@ export interface components {
        * @description Flatetypen til inndelingen
        * @enum {string}
        */
-      type:
-        | "FYLKE"
-        | "KOMMUNE"
-        | "NASJON"
-        | "GRUNNKRETS"
-        | "STEMMEKRETS"
-        | "SKOLEKRETS";
+      type: "FYLKE" | "KOMMUNE" | "NASJON" | "GRUNNKRETS" | "STEMMEKRETS" | "SKOLEKRETS";
       /** @description Navnet til inndelingen */
       navn: string;
       /** @description Nummeret til inndelingen */
@@ -2258,14 +2240,7 @@ export interface operations {
         /** Maksgrense for antall treff man ønsker seg */
         limit?: number;
         /** Hvilke typer inndelinger man ønsker å søke etter */
-        filter?: (
-          | "FYLKE"
-          | "KOMMUNE"
-          | "NASJON"
-          | "GRUNNKRETS"
-          | "STEMMEKRETS"
-          | "SKOLEKRETS"
-        )[];
+        filter?: ("FYLKE" | "KOMMUNE" | "NASJON" | "GRUNNKRETS" | "STEMMEKRETS" | "SKOLEKRETS")[];
       };
     };
     responses: {
@@ -3016,5 +2991,3 @@ export interface operations {
     };
   };
 }
-
-export interface external {}
