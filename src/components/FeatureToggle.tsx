068c533039df7b410a34e3e262cfff3be3367f38
Aleksander Karlsen <aleweskar@gmail.com>
2026-01-19 09:38:56 +0100

    Adjust toggle

--- src/components/FeatureToggle.tsx
+++ src/components/FeatureToggle.tsx
@@ -32,8 +32,8 @@ const featureToggles: Record<Keys, Record<Environment, boolean>> = {
   },
   BOPLIKTOMRADE_EDITING: {
     prod: false,
-    "dev-main": true,
-    "dev-e2e": true,
+    "dev-main": false,
+    "dev-e2e": false,
     localhost: true,
     "feature-branch": true,
   },
