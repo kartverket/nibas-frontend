7d593f6468fa5805d5bc84b57eeff6c3c151bf51
Aleksander Karlsen <aleweskar@gmail.com>
2026-05-13 09:20:24 +0200

    Add rolldown plugin for babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import { checker } from "vite-plugin-checker";
+import babel from "@rolldown/plugin-babel";
 
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
@@ -18,10 +19,9 @@ export default defineConfig(({ mode }) => {
       sourcemap: true,
     },
     plugins: [
-      react({
-        babel: {
-          plugins: [["babel-plugin-react-compiler", { target: "18" }]],
-        },
+      react(),
+      babel({
+        presets: [reactCompilerPreset({ target: "18", compilationMode: "infer" })],
       }),
       viteTsconfigPaths(),
       checker({
