ce4379bcdf775d93b031251ae13bd9cf482da795
Kristian <kristian.haugerud@kartverket.no>
2026-09-09 07:31:04 +0200

    Fjern ubruk TestingLibraryMatchers extension fra Vitest

--- src/test/test-setup.ts
+++ src/test/test-setup.ts
@@ -2,14 +2,7 @@ import "mocks/globals";
 import "@testing-library/jest-dom/vitest";
 import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
-import { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
 import { server } from "../mocks/server";
-
-// extends Vitest's expect method with methods from react-testing-library
-declare module "vitest" {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  interface Assertion<T = any> extends jest.Matchers<void, T>, TestingLibraryMatchers<T, void> {}
-}
 
 beforeAll(() => {
   // Establish API mocking before all tests.
